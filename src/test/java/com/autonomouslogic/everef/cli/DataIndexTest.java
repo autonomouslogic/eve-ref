8a468339ee5c518b5eb6ca036f5ae955621479ca
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-03-19 09:11:35 +0900

    Testing.

--- src/test/java/com/autonomouslogic/everef/cli/DataIndexTest.java
+++ src/test/java/com/autonomouslogic/everef/cli/DataIndexTest.java
@@ -1,7 +1,10 @@
 package com.autonomouslogic.everef.cli;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.autonomouslogic.everef.inject.AwsModule;
@@ -10,18 +13,22 @@ import com.autonomouslogic.everef.s3.S3Adapter;
 import com.autonomouslogic.everef.test.DaggerTestComponent;
 import io.reactivex.rxjava3.core.Flowable;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,7 +57,14 @@ public class DataIndexTest {
 	@SetEnvironmentVariable(key = "DATA_S3_BUCKET", value = "data-bucket")
 	@Test
 	void shouldGenerateIndexPages() {
-		var files = List.of("index.html", "data.zip", "dir/", "dir/index.html", "dir/more-data.zip");
+		var files = List.of(
+				"index.html",
+				"data.zip",
+				"dir/",
+				"dir/index.html",
+				"dir/more-data.zip",
+				"dir2/",
+				"dir2/more-data2.zip");
 		when(s3Adapter.listObjects(any(ListObjectsV2Request.class), eq(dataClient)))
 				.thenReturn(Flowable.fromIterable(List.of(ListObjectsV2Response.builder()
 						.contents(files.stream()
@@ -58,5 +72,10 @@ public class DataIndexTest {
 								.collect(Collectors.toList()))
 						.build())));
 		dataIndex.run().blockingAwait();
+		var putCaptor = ArgumentCaptor.forClass(PutObjectRequest.class);
+		verify(dataClient, times(3)).putObject(putCaptor.capture(), any(AsyncRequestBody.class));
+		var puts =
+				putCaptor.getAllValues().stream().collect(Collectors.toMap(PutObjectRequest::key, Function.identity()));
+		assertEquals(3, puts.size());
 	}
 }
