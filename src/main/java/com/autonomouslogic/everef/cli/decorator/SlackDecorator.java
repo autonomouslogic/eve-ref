277ed1b36c1e56db6ece0c8230804c4517ab3044
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-03-31 00:08:17 +0900

    feat: Slack reporting (#48)

--- src/main/java/com/autonomouslogic/everef/cli/decorator/SlackDecorator.java
+++ src/main/java/com/autonomouslogic/everef/cli/decorator/SlackDecorator.java
@@ -2,15 +2,21 @@ package com.autonomouslogic.everef.cli.decorator;
 
 import com.autonomouslogic.everef.cli.Command;
 import com.autonomouslogic.everef.config.Configs;
+import com.autonomouslogic.everef.util.Rx;
 import io.reactivex.rxjava3.core.Completable;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import net.gpedro.integrations.slack.SlackApi;
+import net.gpedro.integrations.slack.SlackAttachment;
+import net.gpedro.integrations.slack.SlackMessage;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 @Singleton
 @Log4j2
@@ -35,12 +41,70 @@ public class SlackDecorator {
 		return new SlackCommand(command);
 	}
 
-	private void reportSuccess(@NonNull String commandName, Duration runtime) {
-		log.info("SLACK SUCCESS");
+	private Completable report(@NonNull SlackMessage message) {
+		return Completable.fromAction(() -> {
+					if (url.isEmpty()) {
+						return;
+					}
+					log.debug(String.format("Slack message: %s", url.get()));
+					log.trace(String.format("Raw Slack message: %s", message));
+					new SlackApi(url.get()).call(message);
+				})
+				.retry(2, e -> {
+					log.warn(String.format("Slack \"%s\" retrying: %s", url.get(), ExceptionUtils.getMessage(e)));
+					return true;
+				})
+				.onErrorResumeNext(e -> {
+					log.warn(String.format("Slack \"%s\" failed", url.get()), e);
+					return Completable.complete();
+				})
+				.compose(Rx.offloadCompletable());
 	}
 
-	private void reportFailure(@NonNull String commandName, Duration runtime, Throwable error) {
-		log.info("SLACK FAILURE");
+	private Completable reportSuccess(@NonNull String commandName, Duration runtime) {
+		return report(successMessage(
+				String.format("%s completed", commandName),
+				commandName,
+				String.format("%s completed in %s", commandName, runtime.truncatedTo(ChronoUnit.MILLIS))));
+	}
+
+	private Completable reportFailure(@NonNull String commandName, Duration runtime, Throwable error) {
+		return report(errorMessage(
+				String.format("%s failed", commandName),
+				commandName,
+				String.format("%s failed after %s", commandName, runtime.truncatedTo(ChronoUnit.MILLIS)),
+				error));
+	}
+
+	private SlackMessage createMessage() {
+		return new SlackMessage().setUsername(username).setChannel(channel);
+	}
+
+	private SlackMessage successMessage(String text, String title, String message) {
+		SlackMessage msg = createMessage();
+		if (message == null) message = "";
+		msg.setText(text);
+		msg.addAttachments(new SlackAttachment()
+				.setColor("good")
+				.setTitle(title)
+				.setText(message)
+				.setFallback(message));
+		return msg;
+	}
+
+	private SlackMessage errorMessage(String text, String title, String message, Throwable e) {
+		SlackMessage msg = createMessage();
+		if (message == null) message = "";
+		if (e != null) {
+			message += "\n" + ExceptionUtils.getMessage(e);
+		}
+		msg.setText(text);
+		msg.addAttachments(new SlackAttachment()
+				.setColor("danger")
+				.setTitle(title)
+				.setText(message)
+				.setFallback(message));
+		return msg;
 	}
 
 	@RequiredArgsConstructor
@@ -50,17 +114,13 @@ public class SlackDecorator {
 		public Completable run() {
 			return Completable.defer(() -> {
 				var start = Instant.now();
-				return delegate.run()
-						.doOnError(e -> {
-							try {
-								reportFailure(delegate.getName(), Duration.between(start, Instant.now()), e);
-							} finally {
-								throw e;
-							}
-						})
-						.andThen(Completable.fromAction(() -> {
-							reportSuccess(delegate.getName(), Duration.between(start, Instant.now()));
-						}));
+				return Completable.concatArray(
+								delegate.run(),
+								reportSuccess(delegate.getName(), Duration.between(start, Instant.now())))
+						.onErrorResumeNext(e -> {
+							return reportFailure(delegate.getName(), Duration.between(start, Instant.now()), e)
+									.andThen(Completable.error(e));
+						});
 			});
 		}
 
---
+++ src/test/java/com/autonomouslogic/everef/cli/decorator/SlackDecoratorTest.java
@@ -0,0 +1,146 @@
+package com.autonomouslogic.everef.cli.decorator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.autonomouslogic.everef.cli.Command;
+import com.autonomouslogic.everef.test.DaggerTestComponent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.reactivex.rxjava3.core.Completable;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@Log4j2
+@Timeout(5)
+@SetEnvironmentVariable(key = "SLACK_WEBHOOK_CHANNEL", value = "#channel-name")
+@SetEnvironmentVariable(key = "SLACK_WEBHOOK_USERNAME", value = "Slack User")
+public class SlackDecoratorTest {
+	private static final int PORT = 30150;
+
+	@Mock
+	Command testCommand;
+
+	@Inject
+	SlackDecorator slackDecorator;
+
+	@Inject
+	ObjectMapper objectMapper;
+
+	MockWebServer server;
+
+	@BeforeEach
+	@SneakyThrows
+	void before() {
+		DaggerTestComponent.builder().build().inject(this);
+
+		server = new MockWebServer();
+		server.start(PORT);
+		for (int i = 0; i < 2; i++) {
+			server.enqueue(new MockResponse().setResponseCode(204));
+		}
+
+		when(testCommand.run()).thenReturn(Completable.complete());
+		Mockito.lenient().when(testCommand.getName()).thenReturn("command-name");
+	}
+
+	@AfterEach
+	@SneakyThrows
+	void after() {
+		server.close();
+	}
+
+	@Test
+	@SneakyThrows
+	void shouldCallDelegateWhenDisabled() {
+		slackDecorator.decorate(testCommand).run().blockingAwait();
+		verify(testCommand).run();
+		noMoreRequests();
+	}
+
+	@Test
+	@SneakyThrows
+	@SetEnvironmentVariable(key = "SLACK_WEBHOOK_URL", value = "http://localhost:" + PORT + "/webhook?key=val")
+	void shouldReportSuccess() {
+		slackDecorator.decorate(testCommand).run().blockingAwait();
+		verify(testCommand).run();
+		var request = server.takeRequest();
+		var body = assertRequest(request, "/webhook?key=val");
+		var payload = decodePayload(body);
+		log.info("Payload: " + payload);
+		assertEquals("#channel-name", payload.get("channel").asText());
+		assertEquals("Slack User", payload.get("username").asText());
+		assertEquals("command-name completed", payload.get("text").asText());
+		var attachments = payload.get("attachments");
+		assertEquals(1, attachments.size());
+		assertTrue(attachments.get(0).get("fallback").asText().startsWith("command-name completed in PT"));
+		assertTrue(attachments.get(0).get("text").asText().startsWith("command-name completed in PT"));
+		noMoreRequests();
+	}
+
+	@Test
+	@SneakyThrows
+	@SetEnvironmentVariable(key = "SLACK_WEBHOOK_URL", value = "http://localhost:" + PORT + "/webhook?key=val")
+	void shouldReportFailure() {
+		when(testCommand.run()).thenReturn(Completable.error(new RuntimeException("test error message")));
+		var error = assertThrows(
+				RuntimeException.class,
+				() -> slackDecorator.decorate(testCommand).run().blockingAwait());
+		assertEquals("test error message", error.getMessage());
+		verify(testCommand).run();
+		var request = server.takeRequest();
+		var body = assertRequest(request, "/webhook?key=val");
+		var payload = decodePayload(body);
+		log.info("Payload: " + payload);
+		assertEquals("#channel-name", payload.get("channel").asText());
+		assertEquals("Slack User", payload.get("username").asText());
+		assertEquals("command-name failed", payload.get("text").asText());
+		var attachments = payload.get("attachments");
+		assertEquals(1, attachments.size());
+		assertTrue(attachments.get(0).get("fallback").asText().startsWith("command-name failed after PT"));
+		assertTrue(attachments.get(0).get("text").asText().startsWith("command-name failed after PT"));
+		noMoreRequests();
+	}
+
+	private static byte[] assertRequest(RecordedRequest request, String path) {
+		assertEquals(path, request.getPath());
+		assertEquals("POST", request.getMethod());
+		return request.getBody().readByteArray();
+	}
+
+	@SneakyThrows
+	private void noMoreRequests() {
+		assertNull(server.takeRequest(1, TimeUnit.MILLISECONDS));
+	}
+
+	@SneakyThrows
+	private ObjectNode decodePayload(byte[] payload) {
+		var str = new String(payload);
+		if (str.startsWith("payload=")) {
+			str = str.substring(8);
+		}
+		str = URLDecoder.decode(str, StandardCharsets.UTF_8);
+		return (ObjectNode) objectMapper.readTree(str);
+	}
+}
