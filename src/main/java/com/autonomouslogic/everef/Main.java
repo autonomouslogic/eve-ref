7c665fe8dc0a62945847b87cc5952d86ebccd2eb
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-04-03 11:46:22 +0900

    fix: RxJava error handler (#58)

--- src/main/java/com/autonomouslogic/everef/Main.java
+++ src/main/java/com/autonomouslogic/everef/Main.java
@@ -3,6 +3,7 @@ package com.autonomouslogic.everef;
 import com.autonomouslogic.everef.cli.CommandRunner;
 import com.autonomouslogic.everef.config.Configs;
 import com.autonomouslogic.everef.inject.MainComponent;
+import io.reactivex.rxjava3.plugins.RxJavaPlugins;
 import javax.inject.Inject;
 import lombok.extern.log4j.Log4j2;
 
@@ -20,6 +21,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		log.info(String.format("EVE Ref version %s", Configs.EVE_REF_VERSION.getRequired()));
+		RxJavaPlugins.setErrorHandler(e -> log.error("RxJava error", e));
 		try {
 			MainComponent.create().createMain().start(args);
 		} catch (Throwable e) {
