1c04f39012642864b29ab0d204a4b17074e7376c
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-05-08 15:07:39 +0900

    Cleanup.

--- src/main/java/com/autonomouslogic/everef/util/HashUtil.java
+++ src/main/java/com/autonomouslogic/everef/util/HashUtil.java
@@ -1,7 +1,10 @@
 package com.autonomouslogic.everef.util;
 
 import com.google.common.hash.Hashing;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HashUtil {
 	public static final byte[] md5(byte[] in) {
 		return Hashing.md5().hashBytes(in).asBytes();
