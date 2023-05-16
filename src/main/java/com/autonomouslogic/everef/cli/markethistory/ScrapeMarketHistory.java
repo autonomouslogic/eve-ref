ecd9260a831ace7f2fff28a20bbacafbcd26f348
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-05-16 20:33:16 +0900

    10k chunks.

--- src/main/java/com/autonomouslogic/everef/cli/markethistory/ScrapeMarketHistory.java
+++ src/main/java/com/autonomouslogic/everef/cli/markethistory/ScrapeMarketHistory.java
@@ -25,7 +25,6 @@ import java.io.FileOutputStream;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
