1130a1a7df8247c032958f941680f538169079c3
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-05-30 00:01:59 +0900

    Spec.

--- src/main/java/com/autonomouslogic/everef/refdata/MarketGroup.java
+++ src/main/java/com/autonomouslogic/everef/refdata/MarketGroup.java
@@ -4,26 +4,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.Map;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
+
+import java.util.Map;
 
 @Value
 @Builder
 @Jacksonized
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@Schema(description = "An inventory category")
-public class InventoryCategory {
+@Schema(description = "A market group")
+public class MarketGroup {
 	@JsonProperty
-	Long categoryId;
+	Long marketGroupId;
+
+	@JsonProperty
+	Long parentGroupId;
 
 	@JsonProperty
 	Map<String, String> name;
 
 	@JsonProperty
-	Boolean published;
+	Map<String, String> description;
 
 	@JsonProperty
 	Long iconId;
+
+	@JsonProperty
+	Boolean hasTypes;
 }
