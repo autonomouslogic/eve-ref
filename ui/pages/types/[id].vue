0dcef91b04f1c6904d163caf605619b44bfa9948
James Fidler <terrarum@gmail.com>
2023-06-02 11:57:20 +1000

    feat: Basic inventory type pages (#155)
    Co-authored-by: Kenneth Jorgensen <kenneth@autonomouslogic.com>

---
+++ ui/pages/types/[id].vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>id: {{ $route.params.id }}</h2>
+    <img :src="`https://images.evetech.net/types/${inventoryType.type_id}/icon`" alt="">
+
+    <p>Name: {{ inventoryType.name[locale] }}</p>
+    <p>Description: {{ inventoryType.description[locale] }}</p>
+    <p>Price: {{ inventoryType.base_price }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {InventoryType} from "~/api";
+
+  const { locale } = useI18n();
+
+  const route = useRoute();
+  const id = route.params.id;
+
+  if (!id) {
+    console.error('id is null');
+  }
+
+  const response = await useFetch(`https://ref-data.everef.net/types/${id}`);
+  const inventoryType: InventoryType = response.data as InventoryType;
+</script>
+
+<style scoped>
+
+</style>
