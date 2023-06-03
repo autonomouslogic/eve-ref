28cd96635e4d264b294abcb1044998fe18ff814b
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-06-03 20:41:57 +0900

    i18n beta 11.

--- ui/i18n.config.ts
+++ ui/i18n.config.ts
@@ -1,12 +1,30 @@
 import type { I18nOptions } from 'vue-i18n'
 
 export default defineI18nConfig(() => ({
-    locales: ['en'],
+    locales: ['de', 'en', 'es', 'fr', 'ja', 'ru', 'zh'],
     locale: 'en',
     fallbackLocale: 'en',
     messages: {
+        de: {
+            languageName: 'Deutsch',
+        },
         en: {
             languageName: 'English',
-        }
+        },
+        es: {
+            languageName: 'Español',
+        },
+        fr: {
+            languageName: 'Français',
+        },
+        ja: {
+            languageName: '日本語',
+        },
+        ru: {
+            languageName: 'Русский',
+        },
+        zh: {
+            languageName: '中文',
+        },
     }
 } as I18nOptions))
