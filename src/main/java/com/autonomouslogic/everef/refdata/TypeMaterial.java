2d73b90cad1ea7a49478223d72e95fb9c7db5ca6
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-06-19 00:22:46 +0900

    Added type material schema and docs.

---
+++ src/main/java/com/autonomouslogic/everef/refdata/TypeMaterial.java
@@ -0,0 +1,22 @@
+package com.autonomouslogic.everef.refdata;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Schema
+public class TypeMaterial {
+	@JsonProperty
+	Long materialTypeId;
+
+	@JsonProperty
+	Long quantity;
+}
