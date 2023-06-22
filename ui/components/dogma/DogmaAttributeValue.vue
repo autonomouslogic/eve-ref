7301a1c19b3afabdeb70db1f5d311b93f2fee482
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-06-22 22:29:49 +0900

    More null guards.

--- ui/components/dogma/DogmaAttributeValue.vue
+++ ui/components/dogma/DogmaAttributeValue.vue
@@ -13,7 +13,10 @@ const attribute = await refdataApi.getDogmaAttribute({attributeId});
 </script>
 
 <template>
-  <template v-if="attribute.displayName && attribute.displayName[locale]">{{ attribute.displayName[locale] }}</template>
-  <template v-else>{{attribute.name}}</template>
+  <template v-if="attribute">
+    <template v-if="attribute.displayName && attribute.displayName[locale]">{{ attribute.displayName[locale] }}</template>
+    <template v-else>{{attribute.name}}</template>
+  </template>
+  <span v-else>Unknown attribute</span>
   [{{ attributeId }}]: <DogmaValue :value="value" :attribute-id="attributeId" />
 </template>
