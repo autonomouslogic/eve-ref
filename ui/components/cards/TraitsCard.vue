85c58c37beb9857644f3405f2057d5d378a037af
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-07-05 13:58:31 +0900

    feat: Basic reusable cards (#240)

---
+++ ui/components/cards/TraitsCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {InventoryType} from "~/refdata-openapi";
+import CardWrapper from "~/components/cards/CardWrapper.vue";
+import TraitsContainer from "~/components/types/traits/TraitsContainer.vue";
+
+const props = defineProps<{
+	inventoryType: InventoryType
+}>();
+</script>
+
+<template>
+	<CardWrapper title="Traits" v-if="inventoryType.traits">
+		<TraitsContainer :traits="inventoryType.traits"></TraitsContainer>
+	</CardWrapper>
+</template>
