1bb107e04b14de7857e872cac4bc4c06d93398da
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-07-23 11:35:05 +0900

    feat: Variations UI card (#251)

--- ui/components/types/TypeCards.vue
+++ ui/components/types/TypeCards.vue
@@ -6,6 +6,7 @@ import typeCardsConfig from "~/conf/typeCardsConfig";
 import DefaultCard from "~/components/cards/DefaultCard.vue";
 import BasicCard from "~/components/cards/BasicCard.vue";
 import DefensesCard from "~/components/cards/DefensesCard.vue";
+import VariationsCard from "~/components/cards/VariationsCard.vue";
 
 const {locale} = useI18n();
 
@@ -57,6 +58,10 @@ if (Object.keys(dogmaAttributes).length > 0) {
 		<template v-for="(attributes, cardId) in cardAttributes" :key="cardId">
 			<template v-if="typeCardsConfig[cardId].component == 'defenses'"></template>
 			<BasicCard v-else-if="typeCardsConfig[cardId].component == 'basic'"
+				:title="typeCardsConfig[cardId].name[locale]"
+				:inventory-type="inventoryType"
+				:dogma-attributes="attributes" />
+			<VariationsCard v-else-if="typeCardsConfig[cardId].component == 'variations'"
 				:title="typeCardsConfig[cardId].name[locale]"
 				:inventory-type="inventoryType"
 				:dogma-attributes="attributes" />
