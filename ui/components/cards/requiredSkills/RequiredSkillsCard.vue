cdad3cc95c762cd9bb27ba6bdfcdc6c39c726160
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-08-07 00:14:48 +0900

    feat: Required skills UI (#253)

---
+++ ui/components/cards/requiredSkills/RequiredSkillsCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {DogmaAttribute, InventoryType, Skill} from "~/refdata-openapi";
+import CardWrapper from "~/components/cards/CardWrapper.vue";
+import DogmaListItems from "~/components/cards/DogmaListItems.vue";
+import refdataApi from "~/refdata";
+import TypeLink from "~/components/helpers/TypeLink.vue";
+import RequiredSkillsRow from "~/components/cards/requiredSkills/RequiredSkillsRow.vue";
+
+const {locale} = useI18n();
+
+const props = defineProps<{
+	title: string,
+	inventoryType: InventoryType,
+	dogmaAttributes: DogmaAttribute[]
+}>();
+</script>
+
+<template>
+	<template v-if="inventoryType.requiredSkills">
+		<CardWrapper :title="title">
+			<div class="grid grid-cols-3">
+				<RequiredSkillsRow
+					v-for="(level, skillTypeId) in inventoryType.requiredSkills"
+					:key="skillTypeId"
+					:skill-type-id="parseInt(skillTypeId)"
+					:level=level
+					indent="0"
+				/>
+			</div>
+		</CardWrapper>
+	</template>
+</template>
