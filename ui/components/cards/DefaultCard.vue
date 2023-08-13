05df3bbbfb0d2848905c96d08c0ebb87674d99d2
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-08-13 16:59:31 +0900

    feat: Added warning for unknown component types (#254)

--- ui/components/cards/DefaultCard.vue
+++ ui/components/cards/DefaultCard.vue
@@ -6,13 +6,15 @@ import DogmaListItems from "~/components/cards/DogmaListItems.vue";
 const props = defineProps<{
 	title: string,
 	inventoryType: InventoryType,
-	dogmaAttributes: DogmaAttribute[]
+	dogmaAttributes: DogmaAttribute[],
+	component: string
 }>();
 </script>
 
 <template>
-	<template v-if="dogmaAttributes && dogmaAttributes.length > 0">
+	<template v-if="(dogmaAttributes && dogmaAttributes.length > 0) || component">
 		<CardWrapper :title="title">
+			<div v-if="component" class="text-red-700 border-2 border-red-700 p-2 text-center">Unknown component "{{component}}"</div>
 			<ul>
 				<DogmaListItems :inventory-type="inventoryType" :dogma-attributes="dogmaAttributes" />
 			</ul>
---
+++ ui/components/types/TypeCardSelector.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import refdataApi from "~/refdata";
+import {DogmaAttribute, InventoryType} from "~/refdata-openapi";
+import TraitsCard from "~/components/cards/TraitsCard.vue";
+import typeCardsConfig from "~/conf/typeCardsConfig";
+import DefaultCard from "~/components/cards/DefaultCard.vue";
+import BasicCard from "~/components/cards/BasicCard.vue";
+import DefensesCard from "~/components/cards/DefensesCard.vue";
+import ReprocessingCard from "~/components/cards/ReprocessingCard.vue";
+import VariationsCard from "~/components/cards/VariationsCard.vue";
+import RequiredSkillsCard from "~/components/cards/requiredSkills/RequiredSkillsCard.vue";
+
+const props = defineProps<{
+	inventoryType: InventoryType,
+	component: string,
+	title: string,
+	dogmaAttributes: DogmaAttribute[]
+}>();
+</script>
+
+<template>
+	<template v-if="component == 'defenses'"></template>
+	<BasicCard v-else-if="component == 'basic'"
+		:title="title"
+		:inventory-type="inventoryType"
+		:dogma-attributes="dogmaAttributes" />
+	<ReprocessingCard v-else-if="component == 'reprocessing'"
+		:title="title"
+		:inventory-type="inventoryType"
+		:dogma-attributes="dogmaAttributes" />
+	<VariationsCard v-else-if="component == 'variations'"
+		:title="title"
+		:inventory-type="inventoryType"
+		:dogma-attributes="dogmaAttributes" />
+	<RequiredSkillsCard v-else-if="component == 'requiredSkills'"
+		:title="title"
+		:inventory-type="inventoryType"
+		:dogma-attributes="dogmaAttributes" />
+	<DefaultCard v-else
+		:title="title"
+		:component="component"
+		:inventory-type="inventoryType"
+		:dogma-attributes="dogmaAttributes" />
+</template>
