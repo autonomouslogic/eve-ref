3ae98e41ac78659b9116fae9292a31c33ad2c5d6
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-09-09 10:57:48 +0900

    fix: Fixed Font Awesome imports.

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
         '@nuxtjs/i18n',
         '@nuxtjs/tailwindcss',
     ],
+    build: {
+        transpile: [
+            '@fortawesome/vue-fontawesome'
+        ]
+    },
     i18n: { // @todo i18n beta-12 requires this to be in a separate file i18n.config.ts, but beta-12 break Cloudflare Pages, so leaving it here temporarily.
         defaultLocale: 'en',
         vueI18n: {
