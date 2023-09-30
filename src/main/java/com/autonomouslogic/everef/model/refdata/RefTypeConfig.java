afd751c5783e66b89d51e790912607da10e24c00
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-09-30 13:53:09 +0900

    Import test region.

--- src/main/java/com/autonomouslogic/everef/model/refdata/RefTypeConfig.java
+++ src/main/java/com/autonomouslogic/everef/model/refdata/RefTypeConfig.java
@@ -3,6 +3,7 @@ package com.autonomouslogic.everef.model.refdata;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
@@ -15,6 +16,9 @@ public class RefTypeConfig {
 	String file;
 
 	@JsonProperty
+	Pattern fileRegex;
+
+	@JsonProperty
 	Map<String, String> renames;
 
 	@JsonProperty
@@ -25,4 +29,10 @@ public class RefTypeConfig {
 
 	@JsonProperty
 	List<String> removes;
+
+	/**
+	 * If true, the file is a single file containing all the data for this type. If false, the file is an object with many IDs.
+	 */
+	@JsonProperty
+	boolean individualFiles;
 }
