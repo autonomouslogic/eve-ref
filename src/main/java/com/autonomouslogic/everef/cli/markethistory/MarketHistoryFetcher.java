c7df00a06cde1450180a48915c1d41235786476e
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-10-04 02:01:11 +0900

    fix: Market history stats reported correctly (#304)

--- src/main/java/com/autonomouslogic/everef/cli/markethistory/MarketHistoryFetcher.java
+++ src/main/java/com/autonomouslogic/everef/cli/markethistory/MarketHistoryFetcher.java
@@ -56,10 +56,10 @@ public class MarketHistoryFetcher {
 						if (statusCode == 404) {
 							return Flowable.<ObjectNode>empty();
 						} else if (statusCode == 200) {
-							stats.hit(pair);
 							return esiHelper
 									.decodeArrayNode(esiUrl, esiHelper.decodeResponse(response))
-									.map(e -> esiHelper.populateLastModified(e, response));
+									.map(e -> esiHelper.populateLastModified(e, response))
+									.doOnNext(e -> stats.hit(pair));
 						} else {
 							return Flowable.<ObjectNode>error(new RuntimeException(
 									String.format("Unknown status code %s for URL %s", statusCode, esiUrl)));
