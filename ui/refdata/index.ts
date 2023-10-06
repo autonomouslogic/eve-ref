2064811dd8b7288bc677d2b1e37ccafa33b9a844
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-10-06 11:52:33 +0900

    Basic memory caching.

--- ui/refdata/index.ts
+++ ui/refdata/index.ts
@@ -1,8 +1,55 @@
-import {Configuration, FetchAPI, RefdataApi} from "~/refdata-openapi";
+import {
+    Bundle,
+    Configuration,
+    DogmaAttributeToJSON,
+    FetchAPI,
+    InventoryTypeToJSON,
+    RefdataApi
+} from "~/refdata-openapi";
+
+const cache: { [key: string]: string; } = {};
+
+function cacheBundleObj(bundle: Bundle): void {
+    console.log("Bundle types: ", bundle.types);
+    for (let typeId in bundle.types) {
+        const type = bundle.types[typeId];
+        const path = "/types/" + typeId;
+        console.log("Caching ", path);
+        cache[path] = JSON.stringify(InventoryTypeToJSON(type));
+    }
+    console.log("Bundle dogmaAttributes: ", bundle.dogmaAttributes);
+    for (let attributeId in bundle.dogmaAttributes) {
+        const attribute = bundle.dogmaAttributes[attributeId];
+        const path = "/dogma_attributes/" + attributeId;
+        console.log("Caching ", path);
+        cache[path] = JSON.stringify(DogmaAttributeToJSON(attribute));
+    }
+}
+
+// function detectAndCacheBundle(path: string, body: any): void {
+//     if (path.startsWith("/types/")) {
+//         if (path.endsWith("/bundle")) {
+//             var bundle = JSON.parse(JSON.stringify(body)) as Bundle; // Force unwrap Proxy object.
+//             console.log("Detected bundle: ", bundle)
+//             cacheBundle(bundle);
+//         }
+//     }
+// }
 
 const useFetchFetchApi: FetchAPI = async (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => {
-    var url = (input as URL).toString();
-    var response = await useFetch(url);
+    const url = new URL((input as URL).toString()); // Otherwise, it doesn't parse correctly.
+    const path = url.pathname;
+    // Check cache.
+    if (!url.search && cache[path]) {
+        console.log("Cache hit for ", path, cache[path]);
+        return new Response(cache[path] as any);
+    }
+    // Request.
+    console.log("Fetching ", path);
+    var response = await useFetch(url.toString());
+    // // Handle bundle.
+    // detectAndCacheBundle(path, response.data.value);
+    // Return as normal.
     return new Response(JSON.stringify(response.data.value) as any);
 }
 
@@ -13,3 +60,10 @@ const config = new Configuration({
 const refdataApi: RefdataApi = new RefdataApi(config);
 
 export default refdataApi;
+
+export async function cacheBundle(typeId: number): Promise<void> {
+    const bundle = await refdataApi.getTypeBundle({typeId});
+    if (bundle) {
+        cacheBundleObj(bundle);
+    }
+}
