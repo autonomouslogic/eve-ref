023e2233d1df2481cea1d6cffe77341eba1fc2a7
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-10-06 13:06:07 +0900

    Added skills and their types to type bundles.

--- src/main/java/com/autonomouslogic/everef/cli/publishrefdata/TypeBundleRenderer.java
+++ src/main/java/com/autonomouslogic/everef/cli/publishrefdata/TypeBundleRenderer.java
@@ -3,9 +3,11 @@ package com.autonomouslogic.everef.cli.publishrefdata;
 import com.autonomouslogic.everef.mvstore.MVStoreUtil;
 import com.autonomouslogic.everef.refdata.DogmaTypeAttribute;
 import com.autonomouslogic.everef.refdata.InventoryType;
+import com.autonomouslogic.everef.refdata.Skill;
 import com.autonomouslogic.everef.util.RefDataUtil;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Maybe;
 import java.util.Map;
@@ -35,48 +37,98 @@ public class TypeBundleRenderer implements RefDataRenderer {
 	@NonNull
 	private MVStore dataStore;
 
+	private Map<Long, JsonNode> typesMap;
+	private Map<Long, JsonNode> dogmaMap;
+	private Map<Long, JsonNode> skillsMap;
+
 	@Inject
 	protected TypeBundleRenderer() {}
 
 	public Flowable<Pair<String, JsonNode>> render() {
 		return Flowable.defer(() -> {
 			log.info("Creating type bundles");
-			var types = mvStoreUtil.openJsonMap(dataStore, "types", Long.class);
-			var dogma = mvStoreUtil.openJsonMap(dataStore, "dogma_attributes", Long.class);
-			return Flowable.fromIterable(types.keySet()).flatMapMaybe(typeId -> createBundle(types.get(typeId), dogma));
+			typesMap = mvStoreUtil.openJsonMap(dataStore, "types", Long.class);
+			dogmaMap = mvStoreUtil.openJsonMap(dataStore, "dogma_attributes", Long.class);
+			skillsMap = mvStoreUtil.openJsonMap(dataStore, "skills", Long.class);
+			return Flowable.fromIterable(typesMap.keySet()).flatMapMaybe(this::createBundle);
 		});
 	}
 
-	private Maybe<Pair<String, JsonNode>> createBundle(JsonNode type, Map<Long, JsonNode> dogma) {
-		var parsedType = objectMapper.convertValue(type, InventoryType.class);
+	private Maybe<Pair<String, JsonNode>> createBundle(long typeId) {
+		var typeJson = typesMap.get(typeId);
+		var type = objectMapper.convertValue(typeJson, InventoryType.class);
 
-		var path = refDataUtil.subPath("types", parsedType.getTypeId()) + "/bundle";
+		var bundleJson = objectMapper.createObjectNode();
+		var typesJson = bundleJson.putObject("types");
+		var attributesJson = objectMapper.createObjectNode();
+		var skillsJson = objectMapper.createObjectNode();
 
-		var bundle = objectMapper.createObjectNode();
-		var types = bundle.putObject("types");
-		var dogmaAttributes = objectMapper.createObjectNode();
-
-		types.set(Long.toString(parsedType.getTypeId()), type);
-
-		Optional.ofNullable(parsedType.getDogmaAttributes()).stream()
-				.flatMap(e -> e.values().stream())
-				.map(DogmaTypeAttribute::getAttributeId)
-				.forEach(typeAttr -> {
-					var attr = dogma.get(typeAttr);
-					if (attr != null) {
-						dogmaAttributes.set(Long.toString(typeAttr), attr);
-					}
-				});
+		typesJson.set(Long.toString(type.getTypeId()), typeJson);
+		bundleDogmaAttributes(type, attributesJson);
+		bundleVariations(type, typesJson);
+		bundleRequiredSkills(type, skillsJson, typesJson);
 
 		var valid = false;
-		if (!dogmaAttributes.isEmpty()) {
-			bundle.set("dogma_attributes", dogmaAttributes);
+		if (!attributesJson.isEmpty()) {
+			bundleJson.set("dogma_attributes", attributesJson);
+			valid = true;
+		}
+		if (!skillsJson.isEmpty()) {
+			bundleJson.set("skills", skillsJson);
 			valid = true;
 		}
 
 		if (valid) {
-			return Maybe.just(Pair.of(path, bundle));
+			var path = refDataUtil.subPath("types", type.getTypeId()) + "/bundle";
+			return Maybe.just(Pair.of(path, bundleJson));
 		}
 		return Maybe.empty();
+	}
+
+	private void bundleDogmaAttributes(InventoryType type, ObjectNode attributesJson) {
+		Optional.ofNullable(type.getDogmaAttributes()).stream()
+				.flatMap(e -> e.values().stream())
+				.map(DogmaTypeAttribute::getAttributeId)
+				.forEach(typeAttr -> {
+					var attr = dogmaMap.get(typeAttr);
+					if (attr != null) {
+						attributesJson.set(Long.toString(typeAttr), attr);
+					}
+				});
+	}
+
+	private void bundleVariations(InventoryType type, ObjectNode typesJson) {
+		Optional.ofNullable(type.getTypeVariations()).stream()
+				.flatMap(e -> e.values().stream())
+				.flatMap(e -> e.stream())
+				.distinct()
+				.forEach(typeId -> {
+					var json = typesMap.get(typeId);
+					if (json != null) {
+						typesJson.set(Long.toString(typeId), json);
+					}
+				});
+	}
+
+	private void bundleRequiredSkills(InventoryType type, ObjectNode skillsJson, ObjectNode typesJson) {
+		Optional.ofNullable(type.getRequiredSkills()).stream()
+				.flatMap(e -> e.keySet().stream())
+				.distinct()
+				.forEach(skillId -> bundleSkill(skillId, skillsJson, typesJson));
+	}
+
+	private void bundleSkill(long skillId, ObjectNode skillsJson, ObjectNode typesJson) {
+		var skillJson = skillsMap.get(skillId);
+		if (skillJson == null) {
+			return;
+		}
+		skillsJson.set(Long.toString(skillId), skillJson);
+		var skill = objectMapper.convertValue(skillJson, Skill.class);
+		Optional.ofNullable(typesMap.get(skill.getTypeId()))
+				.ifPresent(typeJson -> typesJson.set(Long.toString(skill.getTypeId()), typeJson));
+		Optional.ofNullable(skill.getRequiredSkills()).stream()
+				.flatMap(e -> e.keySet().stream())
+				.distinct()
+				.forEach(id -> bundleSkill(id, skillsJson, typesJson));
 	}
 }
