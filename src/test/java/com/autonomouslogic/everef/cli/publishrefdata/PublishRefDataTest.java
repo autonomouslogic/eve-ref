1fb118de79066b99f91d9c76cea7a7170260f98c
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-10-07 19:02:54 +0900

    Format.

--- src/test/java/com/autonomouslogic/everef/cli/publishrefdata/PublishRefDataTest.java
+++ src/test/java/com/autonomouslogic/everef/cli/publishrefdata/PublishRefDataTest.java
@@ -17,7 +17,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
