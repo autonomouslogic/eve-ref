dfcce65f7840298db6c28b22df3d3a41e944c9aa
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-10-11 16:34:53 +0900

    Time tags on data index.

--- src/main/java/com/autonomouslogic/everef/pug/TimeUtil.java
+++ src/main/java/com/autonomouslogic/everef/pug/TimeUtil.java
@@ -5,13 +5,14 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import javax.inject.Inject;
 
 /**
  *
  */
 public class TimeUtil {
-	private static final DateTimeFormatter isoLike = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+	public static final DateTimeFormatter ISO_LIKE = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
 
 	private static final long DAY = Duration.ofDays(1).toMillis();
 	private static final long HOUR = Duration.ofHours(1).toMillis();
@@ -54,11 +55,15 @@ public class TimeUtil {
 		return sb.toString();
 	}
 
+	public String iso(Instant instant) {
+		return instant.truncatedTo(ChronoUnit.SECONDS).toString();
+	}
+
 	public String isoLike(Instant instant) {
 		return isoLike(instant.atZone(ZoneId.of("UTC")));
 	}
 
 	public String isoLike(ZonedDateTime dateTime) {
-		return isoLike.format(dateTime);
+		return ISO_LIKE.format(dateTime);
 	}
 }
