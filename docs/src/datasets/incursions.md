31249790f0b9a0ca780df3c5fb5a322397f18882
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-10-12 09:59:39 +0900

    eve-incursions.de scrape.

---
+++ docs/src/datasets/incursions.md
@@ -0,0 +1,8 @@
+# Hoboleaks SDE
+
+[data.everef.net/incursions/](https://data.everef.net/incursions/) contains archived history of `/incursions/` endoint on the ESI.
+
+## Backfill
+
+A backfill archive is provided by [eve-incursions.de](https://eve-incursions.de/) at [data.everef.net/incursions/backfills/](https://data.everef.net/incursions/backfills/).
+Thank you to Lars Naurath, the author of that site, for collecting the data and allowing it to be archived here.
---
+++ scripts/eve-incursions-scrape.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -e
+
+# This script scrapes the API at https://eve-incursions.de/ for archival purposes.
+# It's a one-off script, not running regularly.
+# The output is meant as a backfill for the existing incursions data at https://data.everef.net/incursions/
+
+DIR="/tmp/eve-incursions-$(date +%s)"
+echo "Outputting to: $DIR"
+
+
+mkdir -p $DIR
+
+PAGE=1
+while [ true ]
+do
+  echo "Fetching page $PAGE"
+  FILE="$DIR/$PAGE.json"
+  QUERY="{\"query\":\"query{spawnLogs(page: $PAGE){items{id date state spawn {id establishedAt endedAt state constellation {id} influenceLogs{id date influence}}}}}\"}"
+  curl -s -X POST -H 'content-type: application/json' https://eve-incursions.de/api --data "$QUERY" > $FILE
+  ITEMS=$(cat $FILE | jq -r ".data.spawnLogs.items | length")
+  if [ $ITEMS -eq 0 ]; then
+    echo "No more items, exiting"
+    rm $FILE
+    break
+  fi
+  sleep 5
+  PAGE=$((PAGE+1))
+done
+
+FINAL="$DIR/eve-incursions-$(date -u +%Y-%m-%d).json"
+echo "Building output file: $FINAL"
+ls -1 $DIR/*.json | xargs -I{} cat {} | \
+  jq ".data.spawnLogs.items" | \
+  jq -s "add" | \
+  jq "sort_by(.id | tonumber)" > $FINAL
+
+echo "Compressing"
+xz $FINAL
+
+echo "Complete"
