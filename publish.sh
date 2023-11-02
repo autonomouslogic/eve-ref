f61c7d40512c1be2c467705104a41b3533e02ffc
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2023-11-03 00:54:28 +0900

    ci: Removed publish script during release. [skip ci]

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-
-VERSION="$1"
-
-make docker
-docker tag autonomouslogic/eve-ref:$VERSION autonomouslogic/eve-ref:latest
-docker push autonomouslogic/eve-ref:$VERSION
-docker push autonomouslogic/eve-ref:latest
