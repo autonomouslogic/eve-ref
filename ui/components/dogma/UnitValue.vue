ad47b53f371a3d26f02674738166d1d14d8718a2
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-01-05 15:19:51 +0900

    feat: Added group links on attributes [skip release] (#422)

--- ui/components/dogma/UnitValue.vue
+++ ui/components/dogma/UnitValue.vue
@@ -6,6 +6,15 @@ import DefaultUnit from "~/components/dogma/units/DefaultUnit.vue";
 import AbsolutePercent from "~/components/dogma/units/AbsolutePercent.vue";
 import TypeLink from "~/components/helpers/TypeLink.vue";
 import AbsoluteInveresePercent from "~/components/dogma/units/AbsoluteInveresePercent.vue";
+import {
+	ABSOLUTE_PERCENT,
+	ATTRIBUTE_ID,
+	GROUP_ID,
+	INVERSE_ABSOLUTE_PERCENT,
+	SIZE_CLASS,
+	TYPE_ID
+} from "~/lib/unitConstants";
+import GroupLink from "~/components/helpers/GroupLink.vue";
 
 const props = defineProps<{
 	value: number,
@@ -18,11 +27,12 @@ const unit = await refdataApi.getUnit({unitId: props.unitId});
 <template>
 	<span v-if="!unit">{{ props.value }} (Unknown unit ID {{props.unitId}})</span>
 	<template v-else>
-		<AttibuteId v-if="unit.unitId == 119" :value="value" />
-		<Sizeclass v-else-if="unit.unitId == 117" :value="value" />
-		<AbsolutePercent v-else-if="unit.unitId == 127" :value="value" />
-		<AbsoluteInveresePercent v-else-if="unit.unitId == 108" :value="value" />
-		<TypeLink v-else-if="unit.unitId == 116" :type-id="value" />
+		<AttibuteId v-if="unit.unitId == ATTRIBUTE_ID" :value="value" />
+		<Sizeclass v-else-if="unit.unitId == SIZE_CLASS" :value="value" />
+		<AbsolutePercent v-else-if="unit.unitId == ABSOLUTE_PERCENT" :value="value" />
+		<AbsoluteInveresePercent v-else-if="unit.unitId == INVERSE_ABSOLUTE_PERCENT" :value="value" />
+		<GroupLink v-else-if="unit.unitId == GROUP_ID" :group-id="value" />
+		<TypeLink v-else-if="unit.unitId == TYPE_ID" :type-id="value" />
 		<DefaultUnit v-else :unit="unit" :value="value" />
 	</template>
 </template>
