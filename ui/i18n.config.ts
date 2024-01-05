8194bb462d7fa9318497c07504cb777741f2eb4d
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-01-05 11:55:48 +0900

    chore(deps): Updated @nuxtjs/i18n to 8.0.0 (#420)

---
+++ ui/i18n.config.ts
@@ -0,0 +1,27 @@
+export default defineI18nConfig(() => ({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+        de: {
+            languageName: 'Deutsch',
+        },
+        en: {
+            languageName: 'English',
+        },
+        es: {
+            languageName: 'Español',
+        },
+        fr: {
+            languageName: 'Français',
+        },
+        ja: {
+            languageName: '日本語',
+        },
+        ru: {
+            languageName: 'Русский',
+        },
+        zh: {
+            languageName: '中文',
+        },
+    }
+}));
