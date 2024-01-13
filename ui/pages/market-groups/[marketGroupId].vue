b594143ae86b8ea8f868498b589bb4fc38394e74
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-01-14 01:03:14 +0900

    Tweaked inventory and market group headers

--- ui/pages/market-groups/[marketGroupId].vue
+++ ui/pages/market-groups/[marketGroupId].vue
@@ -5,6 +5,7 @@ import TypeLink from "~/components/helpers/TypeLink.vue";
 import MarketGroupLink from "~/components/helpers/MarketGroupLink.vue";
 import MarketGroupBreadcrumbs from "~/components/helpers/MarketGroupBreadcrumbs.vue";
 import {getIntRouteParam} from "~/lib/routeUtils";
+import CategoryLink from "~/components/helpers/CategoryLink.vue";
 
 const route = useRoute();
 const {locale} = useI18n();
@@ -19,9 +20,9 @@ useHead({
 <template>
 	<div>
 		<h1 v-if="marketGroup.name">{{ marketGroup.name[locale] }}</h1>
-		<p v-if="marketGroup.parentGroupId">
-			<MarketGroupBreadcrumbs :market-group-id="marketGroup.parentGroupId"></MarketGroupBreadcrumbs>
-		</p>
+		<div v-if="marketGroup.parentGroupId" class="mb-3">
+			Market group: <MarketGroupBreadcrumbs :market-group-id="marketGroup.parentGroupId" />
+		</div>
 		<ul>
 			<li v-for="marketGroupId in marketGroup.childMarketGroupIds" :key="marketGroupId">
 				<MarketGroupLink :marketGroupId="marketGroupId"></MarketGroupLink>
