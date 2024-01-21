1f1f24fee4b4aa6addda720ef48d350a3dcb91df
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-01-21 15:48:36 +0900

    fix(ui): Client cards via inner wrappers (#450)

---
+++ ui/components/cards/InsuranceCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {type DogmaAttribute, type InventoryType} from "~/refdata-openapi";
+import CardWrapper from "~/components/cards/CardWrapper.vue";
+import refdataApi from "~/refdata";
+import {SHIP} from "~/lib/categoryConstants";
+
+const props = defineProps<{
+	title: string,
+	inventoryType: InventoryType,
+	dogmaAttributes: DogmaAttribute[]
+}>();
+
+const group = !props.inventoryType.groupId ? undefined :
+	await refdataApi.getGroup({groupId: props.inventoryType.groupId});
+const isShip = group?.categoryId == SHIP;
+</script>
+
+<template>
+	<CardWrapper :title="title" v-if="isShip">
+		<CardsInsuranceCardInner :inventory-type="inventoryType" />
+	</CardWrapper>
+</template>
---
+++ ui/components/cards/market/MarketCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {type DogmaAttribute, type InventoryType} from "~/refdata-openapi";
+import CardWrapper from "~/components/cards/CardWrapper.vue";
+import MarketLinks from "~/components/cards/market/MarketLinks.vue";
+
+const props = defineProps<{
+	title: string,
+	inventoryType: InventoryType,
+	dogmaAttributes: DogmaAttribute[]
+}>();
+</script>
+
+<template>
+	<template v-if="inventoryType.typeId && inventoryType.marketGroupId">
+		<CardWrapper :title="title">
+			<CardsMarketCardInner :inventory-type="inventoryType" :type-id="inventoryType.typeId" />
+			<MarketLinks :inventory-type="inventoryType" />
+		</CardWrapper>
+	</template>
+</template>
