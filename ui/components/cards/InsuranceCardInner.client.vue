7ee7df9184712c56829c63aa8ebe54d322e3abf4
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-01-21 15:17:52 +0900

    fix(ui): Client cards via inner wrappers

---
+++ ui/components/cards/InsuranceCardInner.client.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {type InventoryType} from "~/refdata-openapi";
+import {insuranceApi} from "~/esi";
+import UnitValue from "~/components/dogma/UnitValue.vue";
+import FormattedNumber from "~/components/helpers/FormattedNumber.vue";
+import {getJitaSellPrice} from "~/lib/marketUtils";
+import {MONEY} from "~/lib/unitConstants";
+
+const props = defineProps<{
+	inventoryType: InventoryType
+}>();
+
+const prices = (await insuranceApi.getInsurancePrices())
+	.filter(e => e.typeId == props.inventoryType.typeId)
+	.pop();
+const marketPrice = props.inventoryType.typeId ? await getJitaSellPrice(props.inventoryType.typeId) : undefined;
+</script>
+
+<template>
+	<table v-if="prices">
+		<thead>
+			<tr>
+				<th>Level</th>
+				<th>Cost</th>
+				<th>Est. payout</th>
+				<th v-if="marketPrice">Coverage</th>
+				<th>Payout over cost</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="level in prices.levels" :key="level.name">
+				<td>{{ level.name }}</td>
+				<td><UnitValue :unit-id="MONEY" :value="level.cost" /></td>
+				<td><UnitValue :unit-id="MONEY" :value="level.payout" /></td>
+				<td v-if="marketPrice"><FormattedNumber :number="level.payout / marketPrice * 100" :decimals="1" />%</td>
+				<td><FormattedNumber :number="level.payout / level.cost" :decimals="1" />x</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
---
+++ ui/components/cards/market/MarketCardInner.client.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {HUB_STATION_IDS} from "~/lib/marketUtils";
+import MarketRow from "~/components/cards/market/MarketRow.vue";
+
+const props = defineProps<{
+	typeId: number
+}>();
+
+const stationIds = HUB_STATION_IDS.values();
+</script>
+
+<template>
+	<table class="table table-auto w-full">
+		<thead>
+			<tr>
+				<th class="text-left">Hub</th>
+				<th class="text-right">Sell</th>
+				<th class="text-right">Buy</th>
+			</tr>
+		</thead>
+		<tbody>
+			<MarketRow v-for="stationId in stationIds" :key="stationId" :type-id="typeId" :station-id="stationId" />
+		</tbody>
+	</table>
+</template>
