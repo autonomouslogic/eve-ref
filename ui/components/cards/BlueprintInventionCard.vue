6d71b4d4c8b7b522869f56b130ce617d4019e83e
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-01-21 13:34:10 +0900

    Invention card

--- ui/components/cards/BlueprintInventionCard.vue
+++ ui/components/cards/BlueprintInventionCard.vue
@@ -7,6 +7,10 @@ import refdataApi from "~/refdata";
 import BlueprintManufacturingLinks from "~/components/cards/BlueprintManufacturingLinks.vue";
 import Duration from "~/components/dogma/units/Duration.vue";
 import {secondsToMilliseconds} from "~/lib/timeUtils";
+import AttributeList from "~/components/attr/AttributeList.vue";
+import AttributeListItem from "~/components/attr/AttributeListItem.vue";
+import RequiredSkillsRow from "~/components/cards/requiredSkills/RequiredSkillsRow.vue";
+import AbsolutePercent from "~/components/dogma/units/AbsolutePercent.vue";
 
 const props = defineProps<{
 	title: string,
@@ -14,48 +18,63 @@ const props = defineProps<{
 	dogmaAttributes: DogmaAttribute[]
 }>();
 
-let blueprintType: InventoryType | undefined = props.inventoryType.isBlueprint ? props.inventoryType : undefined;
-
-let blueprint: Blueprint | undefined;
-let manufacturing: BlueprintActivity | undefined;
-if (blueprintType?.typeId) {
-	blueprint = await refdataApi.getBlueprint({blueprintTypeId: blueprintType.typeId});
-	const activities = blueprint.activities;
-	if (activities) {
-		manufacturing = activities.manufacturing;
-	}
-}
+const blueprintType = props.inventoryType.isBlueprint ? props.inventoryType : undefined;
+const blueprint = blueprintType?.typeId ? await refdataApi.getBlueprint({blueprintTypeId: blueprintType.typeId}) : undefined;
+const invention = blueprint?.activities?.invention;
 
 </script>
 
 <template>
-	<template v-if="manufacturing">
+	<template v-if="invention">
 		<CardWrapper :title="title">
-			<table>
-				<tr>
-					<td>Manufacturing time:</td>
-					<td><Duration :milliseconds="secondsToMilliseconds(manufacturing.time)" /></td>
+			<AttributeList v-if="invention.time">
+				<AttributeListItem>
+					<template v-slot:key>Copy time:</template>
+					<Duration :milliseconds="invention.time * 1000" />
+				</AttributeListItem>
+			</AttributeList>
+
+			<template v-if="invention.products">
+				<h3>Output</h3>
+				<table class="w-full">
+					<thead>
+						<th class="text-left"></th>
+						<th class="text-right">Probability</th>
+						<th class="text-right">Quantity</th>
+					</thead>
+					<tr v-for="(product, index) in invention.products" :key="index">
+						<td class="text-left"><TypeLink :type-id="product.typeId" /></td>
+						<td class="text-right"><AbsolutePercent v-if="product.probability" :value="product.probability" /></td>
+						<td class="text-right"><FormattedNumber :number="product.quantity" /></td>
+					</tr>
+				</table>
+			</template>
+
+			<h3>Input</h3>
+			<table class="w-full">
+				<thead>
+					<th class="text-left"></th>
+					<th class="text-right">Quantity</th>
+				</thead>
+				<tr v-for="(material, index) in invention.materials" :key="index">
+					<td class="text-left"><TypeLink :type-id="material.typeId" /></td>
+					<td class="text-right"><FormattedNumber :number="material.quantity" /></td>
 				</tr>
-				<tr>
-					<td colspan="2">Products</td>
-				</tr>
-				<template v-if="manufacturing.products">
-					<tr v-for="(product, productId) in manufacturing.products" :key="productId">
-						<td><TypeLink :type-id="product.typeId" /></td>
-						<td><FormattedNumber :number="product.quantity" /></td>
-					</tr>
-				</template>
-				<template v-if="manufacturing.materials">
-					<tr>
-						<td colspan="2">Ingredients</td>
-					</tr>
-					<tr v-for="(material, materialId) in manufacturing.materials" :key="materialId">
-						<td><TypeLink :type-id="material.typeId" /></td>
-						<td><FormattedNumber :number="material.quantity" /></td>
-					</tr>
-				</template>
 			</table>
-			<BlueprintManufacturingLinks v-if="blueprint" :blueprint="blueprint" />
+
+			<template v-if="invention.requiredSkills">
+				<h3>Required Skills</h3>
+				<div class="grid grid-cols-3">
+					<RequiredSkillsRow
+						v-for="(level, requiredSkillTypeId) in invention.requiredSkills"
+						:key="requiredSkillTypeId"
+						:skill-type-id="parseInt(`${requiredSkillTypeId}`)"
+						:level=level
+						:indent="0"
+						:shown-skills="[]"
+					/>
+				</div>
+			</template>
 		</CardWrapper>
 	</template>
 </template>
