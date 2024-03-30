d432cd540f1e7dd849a993954e390bd809c7258d
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-03-31 00:09:06 +0900

    Docs

--- src/main/java/com/autonomouslogic/everef/cli/refdata/post/CanFitDecorator.java
+++ src/main/java/com/autonomouslogic/everef/cli/refdata/post/CanFitDecorator.java
@@ -17,9 +17,7 @@ import lombok.extern.log4j.Log4j2;
 
 /**
  * <ul>
- *     <li>Populates mutaplasmid data for inventory types.</li>
- *     <li>Populates dynamic item data for inventory types.</li>
- *     <li>Creates mutaplasmid entries.</li>
+ *     <li>Cross-references <code>canFitShipType#</code> dogma attributes.</li>
  * </ul>
  *
  */
