72d2fd243fd31a13eebc528041cc0af558b471c9
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-04-20 22:03:29 +0900

    Skill point overview

---
+++ ui/pages/skill-points.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ExternalLink from "~/components/helpers/ExternalLink.vue";
+
+useHead({
+  title: "Skill points"
+});
+</script>
+
+<template>
+  <table class="table-auto text-left">
+    <thead>
+    <th>Source</th>
+    <th>Price</th>
+    <th>ISK / SP</th>
+    </thead>
+    <tbody>
+    <tr>
+      <td>Alpha account</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
