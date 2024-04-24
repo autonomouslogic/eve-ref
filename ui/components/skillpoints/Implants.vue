0a0326ee17d92d26949cfa0c226e9a55e52f87a7
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-04-24 15:21:01 +0900

    Sort

--- ui/components/skillpoints/Implants.vue
+++ ui/components/skillpoints/Implants.vue
@@ -51,58 +51,15 @@ class ImplantIskPerSp {
 }
 
 const sets: ImplantSet[] = [
-	// {
-	//   name: "Elite",
-	//   bonus: 7,
-	//   typeIds: [
-	//     OCULAR_FILTER_ELITE,
-	//     MEMORY_AUGMENTATION_ELITE,
-	//     NEURAL_BOOST_ELITE,
-	//     CYBERNETIC_SUBPROCESSOR_ELITE,
-	//   ]
-	// } as ImplantSet,
-	// {
-	//   name: "Advanced",
-	//   bonus: 6,
-	//   typeIds: [
-	//     OCULAR_FILTER_ADVANCED,
-	//     MEMORY_AUGMENTATION_ADVANCED,
-	//     NEURAL_BOOST_ADVANCED,
-	//     CYBERNETIC_SUBPROCESSOR_ADVANCED,
-	//     SOCIAL_ADAPTATION_CHIP_ADVANCED,
-	//   ]
-	// } as ImplantSet,
 	{
-		name: "Improved",
-		bonus: 5,
+		name: "Limited",
+		bonus: 1,
 		typeIds: [
-			OCULAR_FILTER_IMPROVED,
-			MEMORY_AUGMENTATION_IMPROVED,
-			NEURAL_BOOST_IMPROVED,
-			CYBERNETIC_SUBPROCESSOR_IMPROVED,
-			SOCIAL_ADAPTATION_CHIP_IMPROVED,
-		]
-	} as ImplantSet,
-	{
-		name: "Standard",
-		bonus: 4,
-		typeIds: [
-			OCULAR_FILTER_STANDARD,
-			MEMORY_AUGMENTATION_STANDARD,
-			NEURAL_BOOST_STANDARD,
-			CYBERNETIC_SUBPROCESSOR_STANDARD,
-			SOCIAL_ADAPTATION_CHIP_STANDARD,
-		]
-	} as ImplantSet,
-	{
-		name: "Basic",
-		bonus: 3,
-		typeIds: [
-			OCULAR_FILTER_BASIC,
-			MEMORY_AUGMENTATION_BASIC,
-			NEURAL_BOOST_BASIC,
-			CYBERNETIC_SUBPROCESSOR_BASIC,
-			SOCIAL_ADAPTATION_CHIP_BASIC,
+			LIMITED_OCULAR_FILTER,
+			LIMITED_MEMORY_AUGMENTATION,
+			LIMITED_NEURAL_BOOST,
+			LIMITED_CYBERNETIC_SUBPROCESSOR,
+			LIMITED_SOCIAL_ADAPTATION_CHIP,
 		]
 	} as ImplantSet,
 	{
@@ -117,16 +74,59 @@ const sets: ImplantSet[] = [
 		]
 	} as ImplantSet,
 	{
-		name: "Limited",
-		bonus: 1,
+		name: "Basic",
+		bonus: 3,
 		typeIds: [
-			LIMITED_OCULAR_FILTER,
-			LIMITED_MEMORY_AUGMENTATION,
-			LIMITED_NEURAL_BOOST,
-			LIMITED_CYBERNETIC_SUBPROCESSOR,
-			LIMITED_SOCIAL_ADAPTATION_CHIP,
+			OCULAR_FILTER_BASIC,
+			MEMORY_AUGMENTATION_BASIC,
+			NEURAL_BOOST_BASIC,
+			CYBERNETIC_SUBPROCESSOR_BASIC,
+			SOCIAL_ADAPTATION_CHIP_BASIC,
 		]
 	} as ImplantSet,
+	{
+		name: "Standard",
+		bonus: 4,
+		typeIds: [
+			OCULAR_FILTER_STANDARD,
+			MEMORY_AUGMENTATION_STANDARD,
+			NEURAL_BOOST_STANDARD,
+			CYBERNETIC_SUBPROCESSOR_STANDARD,
+			SOCIAL_ADAPTATION_CHIP_STANDARD,
+		]
+	} as ImplantSet,
+	{
+		name: "Improved",
+		bonus: 5,
+		typeIds: [
+			OCULAR_FILTER_IMPROVED,
+			MEMORY_AUGMENTATION_IMPROVED,
+			NEURAL_BOOST_IMPROVED,
+			CYBERNETIC_SUBPROCESSOR_IMPROVED,
+			SOCIAL_ADAPTATION_CHIP_IMPROVED,
+		]
+	} as ImplantSet,
+	// {
+	//   name: "Advanced",
+	//   bonus: 6,
+	//   typeIds: [
+	//     OCULAR_FILTER_ADVANCED,
+	//     MEMORY_AUGMENTATION_ADVANCED,
+	//     NEURAL_BOOST_ADVANCED,
+	//     CYBERNETIC_SUBPROCESSOR_ADVANCED,
+	//     SOCIAL_ADAPTATION_CHIP_ADVANCED,
+	//   ]
+	// } as ImplantSet,
+	// {
+	//   name: "Elite",
+	//   bonus: 7,
+	//   typeIds: [
+	//     OCULAR_FILTER_ELITE,
+	//     MEMORY_AUGMENTATION_ELITE,
+	//     NEURAL_BOOST_ELITE,
+	//     CYBERNETIC_SUBPROCESSOR_ELITE,
+	//   ]
+	// } as ImplantSet,
 ];
 
 // Load prices.
