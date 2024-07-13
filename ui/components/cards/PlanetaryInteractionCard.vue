546e449cc54ad8ed261fa5d269f4499050ee8df2
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-07-14 03:32:01 +0900

    More things

--- ui/components/cards/PlanetaryInteractionCard.vue
+++ ui/components/cards/PlanetaryInteractionCard.vue
@@ -29,66 +29,101 @@ if (props.inventoryType.producedBySchematicIds) {
 		producedBySchematics.push(schematic);
 	}
 }
+
+const usedBySchematics: Schematic[] = [];
+if (props.inventoryType.usedBySchematicIds) {
+	for (let schematicId of props.inventoryType.usedBySchematicIds) {
+		const schematic = await refdataApi.getSchematic({schematicId});
+		usedBySchematics.push(schematic);
+	}
+}
+
 </script>
 
 <template>
-	<CardWrapper :title="title">
-		<AttributeList>
-			<DogmaListItems :inventory-type="inventoryType" :dogma-attributes="dogmaAttributes" />
-		</AttributeList>
+	<template v-if="dogmaAttributes.length > 0 || producedBySchematics.length > 0 || inventoryType.harvestedByPinTypeIds || usedBySchematics.length > 0">
 
-		<template v-if="producedBySchematics.length > 0">
-        
-			<template v-for="schematic in producedBySchematics" :key="schematic.schematicId">
-				<table class="w-full">
-					<tr v-if="schematic.name?.en">
-						<td>Schematic</td>
-						<td>{{ schematic.name.en }} [{{schematic.schematicId}}]</td>
-					</tr>
-					<tr v-if="schematic.cycleTime">
-						<td>Cycle time</td>
-						<td><Duration :milliseconds="schematic.cycleTime * 1000" /></td>
-					</tr>
-				</table>
+		<CardWrapper :title="title">
+			<AttributeList>
+				<DogmaListItems :inventory-type="inventoryType" :dogma-attributes="dogmaAttributes" />
+			</AttributeList>
 
-				<table v-if="schematic.products" class="w-full mt-3">
-					<thead>
-						<tr>
-							<th>Product</th>
-							<th>Quantity</th>
+			<template v-if="producedBySchematics.length > 0">
+
+				<template v-for="schematic in producedBySchematics" :key="schematic.schematicId">
+					<table class="w-full">
+						<tr v-if="schematic.name?.en">
+							<td>Schematic</td>
+							<td>{{ schematic.name.en }} [{{schematic.schematicId}}]</td>
 						</tr>
-					</thead>
-					<tr v-for="(product, productId) in schematic.products" :key="productId">
-						<td><TypeLink :type-id="product.typeId" /></td>
-						<td><FormattedNumber :number="product.quantity" /></td>
-					</tr>
-				</table>
-
-				<table v-if="schematic.materials" class="w-full mt-3">
-					<thead>
-						<tr>
-							<th>Material</th>
-							<th>Quantity</th>
+						<tr v-if="schematic.cycleTime">
+							<td>Cycle time</td>
+							<td><Duration :milliseconds="schematic.cycleTime * 1000" /></td>
 						</tr>
-					</thead>
-					<tr v-for="(material, materialId) in schematic.materials" :key="materialId">
-						<td><TypeLink :type-id="material.typeId" /></td>
-						<td><FormattedNumber :number="material.quantity" /></td>
-					</tr>
-				</table>
+					</table>
 
-				<div v-if="schematic.pinTypeIds && schematic.pinTypeIds.length > 0" class="mt-3">
-					<b>Produced in:</b>
-					<ul>
-						<li v-for="pinTypeId in schematic.pinTypeIds" :key="pinTypeId">
-							<TypeLink :type-id="pinTypeId" />
-						</li>
-					</ul>
-				</div>
+					<table v-if="schematic.products" class="w-full mt-3">
+						<thead>
+							<tr>
+								<th>Product</th>
+								<th>Quantity</th>
+							</tr>
+						</thead>
+						<tr v-for="(product, productId) in schematic.products" :key="productId">
+							<td><TypeLink :type-id="product.typeId" /></td>
+							<td><FormattedNumber :number="product.quantity" /></td>
+						</tr>
+					</table>
 
+					<table v-if="schematic.materials" class="w-full mt-3">
+						<thead>
+							<tr>
+								<th>Material</th>
+								<th>Quantity</th>
+							</tr>
+						</thead>
+						<tr v-for="(material, materialId) in schematic.materials" :key="materialId">
+							<td><TypeLink :type-id="material.typeId" /></td>
+							<td><FormattedNumber :number="material.quantity" /></td>
+						</tr>
+					</table>
+
+					<div v-if="schematic.pinTypeIds && schematic.pinTypeIds.length > 0" class="mt-3">
+						<b>Produced in:</b>
+						<ul>
+							<li v-for="pinTypeId in schematic.pinTypeIds" :key="pinTypeId">
+								<TypeLink :type-id="pinTypeId" />
+							</li>
+						</ul>
+					</div>
+
+				</template>
 			</template>
-		</template>
-	</CardWrapper>
+
+			<div v-if="inventoryType.harvestedByPinTypeIds && inventoryType.harvestedByPinTypeIds.length > 0" class="mt-3">
+				<b>Harvested by:</b>
+				<ul>
+					<li v-for="pinTypeId in inventoryType.harvestedByPinTypeIds" :key="pinTypeId">
+						<TypeLink :type-id="pinTypeId" />
+					</li>
+				</ul>
+			</div>
+
+			<div v-if="usedBySchematics.length > 0" class="mt-3">
+				<b>Used by schematics:</b>
+				<ul>
+					<template v-for="schematic in usedBySchematics" :key="schematic.schematicId">
+						<template v-if="schematic.products">
+							<li v-for="(product, productId) in schematic.products" :key="productId">
+								<TypeLink :type-id="product.typeId" />
+							</li>
+						</template>
+					</template>
+				</ul>
+			</div>
+
+		</CardWrapper>
+	</template>
 </template>
 
 <style>
