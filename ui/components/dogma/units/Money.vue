20118b017798a30486ccf2abd32bc7d4f395dabd
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-07-15 00:06:37 +0900

    fix(ui): Changed money to include thousands and shorten anything over the limit

--- ui/components/dogma/units/Money.vue
+++ ui/components/dogma/units/Money.vue
@@ -12,13 +12,21 @@ const format = new Intl.NumberFormat("en-US", {
 const formatted = computed(() => {
 	let v = props.value;
 	let s = "";
-	if (v > 1.5e9) {
+	if (v >= 1e12) {
+		v = props.value / 1e12;
+		s = "t";
+	}
+	else if (v >= 1e9) {
 		v = props.value / 1e9;
 		s = "b";
 	}
-	else if (v > 1.5e6) {
+	else if (v >= 1e6) {
 		v = props.value / 1e6;
 		s = "m";
+	}
+	else if (v >= 1e3) {
+		v = props.value / 1e3;
+		s = "k";
 	}
 	return `${format.format(v)}${s} ISK`;
 });
