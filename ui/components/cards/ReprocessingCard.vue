67092b9ecf44f035112afe9d0090e9a2c592b532
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-09-10 01:18:27 +0900

    fix(ui): Added reprocessing skill

--- ui/components/cards/ReprocessingCard.vue
+++ ui/components/cards/ReprocessingCard.vue
@@ -3,6 +3,9 @@ import {type DogmaAttribute, type InventoryType} from "~/refdata-openapi";
 import CardWrapper from "~/components/cards/CardWrapper.vue";
 import TypeLink from "~/components/helpers/TypeLink.vue";
 import FormattedNumber from "~/components/helpers/FormattedNumber.vue";
+import DogmaListItems from "~/components/cards/DogmaListItems.vue";
+import AttributeListItem from "~/components/attr/AttributeListItem.vue";
+import AttributeList from "~/components/attr/AttributeList.vue";
 
 const props = defineProps<{
 	title: string,
@@ -14,8 +17,19 @@ const props = defineProps<{
 <template>
 	<template v-if="inventoryType.typeMaterials">
 		<CardWrapper :title="title">
+			<AttributeList>
+				<DogmaListItems :inventory-type="inventoryType" :dogma-attributes="dogmaAttributes" />
+				<AttributeListItem v-if="inventoryType.portionSize">
+					<template v-slot:key>Portion size:</template>
+					<FormattedNumber :number="inventoryType.portionSize" />
+				</AttributeListItem>
+			</AttributeList>
+
 			<table class="table-auto text-left w-full">
 				<tbody>
+					<tr>
+						<td colspan="2">Output</td>
+					</tr>
 					<tr v-for="(material, materialId) in inventoryType.typeMaterials" :key="materialId">
 						<td>
 							<TypeLink :type-id="materialId" />
