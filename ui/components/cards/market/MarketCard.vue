f2ab91e17f7672c272611214e018729858df796d
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-09-21 02:22:42 +0900

    fix(ui): Market prices only load on client (#597)

--- ui/components/cards/market/MarketCard.vue
+++ ui/components/cards/market/MarketCard.vue
@@ -2,6 +2,7 @@
 import {type DogmaAttribute, type InventoryType} from "~/refdata-openapi";
 import CardWrapper from "~/components/cards/CardWrapper.vue";
 import MarketLinks from "~/components/cards/market/MarketLinks.vue";
+import MarketCardInner from "~/components/cards/market/MarketCardInner.vue";
 
 const props = defineProps<{
 	title: string,
@@ -13,7 +14,9 @@ const props = defineProps<{
 <template>
 	<template v-if="inventoryType.typeId && inventoryType.marketGroupId">
 		<CardWrapper :title="title">
-			<CardsMarketCardInner :inventory-type="inventoryType" :type-id="inventoryType.typeId" />
+			<ClientOnly>
+				<MarketCardInner :inventory-type="inventoryType" :type-id="inventoryType.typeId" />
+			</ClientOnly>
 			<MarketLinks :inventory-type="inventoryType" />
 		</CardWrapper>
 	</template>
