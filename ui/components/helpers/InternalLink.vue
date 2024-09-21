e4b854e05c79dabc65e4f58f80bb2b4a2c0685f8
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-09-21 13:50:07 +0900

    Missing file

---
+++ ui/components/helpers/InternalLink.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const localePath = useLocalePath();
+
+const props = defineProps<{
+	to: string
+}>();
+
+
+
+</script>
+
+<template>
+	<NuxtLink
+		:to="localePath(to)">
+		<slot />
+	</NuxtLink>
+</template>
