ab07bb69eb851294159facdc466c3d3391e0ae09
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-09-29 13:53:51 +0900

    Optimised

--- ui/lib/marketUtils.ts
+++ ui/lib/marketUtils.ts
@@ -1,31 +1,51 @@
 import {GetMarketsRegionIdOrdersDatasourceEnum, GetMarketsRegionIdOrdersOrderTypeEnum} from "~/esi-openapi";
 import {marketApi} from "~/esi";
 import type {DogmaAttribute, InventoryType} from "~/refdata-openapi";
+import {DOMAIN, HEIMATAR, METROPOLIS, SINQ_LAISON, THE_FORGE} from "~/lib/regionConstants";
 
 export interface HubStation {
     systemName: string,
-    shortName: string,
     stationId: number,
     regionId: number
 }
 
-export const THE_FORGE_REGION_ID = 10000002;
-
-export const HUB_STATION_IDS = new Map<string, HubStation>();
+export const HUB_STATIONS = new Map<string, HubStation>();
 // Jita IV - Moon 4 - Caldari Navy Assembly Plant
-HUB_STATION_IDS.set("Jita", {
+HUB_STATIONS.set("Jita", {
     systemName: "Jita",
-    shortName: "Jita",
     stationId: 60003760,
-    regionId: THE_FORGE_REGION_ID
+    regionId: THE_FORGE
 } as HubStation);
-
-HUB_STATION_IDS.set("Jita", 60003760); // Jita IV - Moon 4 - Caldari Navy Assembly Plant
-HUB_STATION_IDS.set("Amarr", 60008494); // Amarr VIII (Oris) - Emperor Family Academy
-HUB_STATION_IDS.set("Rens", 60004588); // Rens VI - Moon 8 - Brutor Tribe Treasury
-HUB_STATION_IDS.set("Dodixie", 60011866); // Dodixie IX - Moon 20 - Federation Navy Assembly Plant
-HUB_STATION_IDS.set("Hek", 60005686); // Hek VIII - Moon 12 - Boundless Creation Factory
-HUB_STATION_IDS.set("Misaba", 60006658); // Misaba V - Moon 3 - Zoar and Sons Factory
+// Amarr VIII (Oris) - Emperor Family Academy
+HUB_STATIONS.set("Amarr", {
+    systemName: "Amarr",
+    stationId: 60008494,
+    regionId: DOMAIN
+} as HubStation);
+// Rens VI - Moon 8 - Brutor Tribe Treasury
+HUB_STATIONS.set("Rens", {
+    systemName: "Rens",
+    stationId: 60004588,
+    regionId: HEIMATAR
+} as HubStation);
+// Dodixie IX - Moon 20 - Federation Navy Assembly Plant
+HUB_STATIONS.set("Dodixie", {
+    systemName: "Dodixie",
+    stationId: 60011866,
+    regionId: SINQ_LAISON
+} as HubStation);
+// Hek VIII - Moon 12 - Boundless Creation Factory
+HUB_STATIONS.set("Hek", {
+    systemName: "Hek",
+    stationId: 60005686,
+    regionId: METROPOLIS
+} as HubStation);
+// Misaba V - Moon 3 - Zoar and Sons Factory
+HUB_STATIONS.set("Misaba", {
+    systemName: "Misaba",
+    stationId: 60006658,
+    regionId: DOMAIN
+} as HubStation);
 
 export async function getOrders(orderType: GetMarketsRegionIdOrdersOrderTypeEnum, typeId: number, regionId: number) {
     return marketApi.getMarketsRegionIdOrders({
@@ -37,9 +57,12 @@ export async function getOrders(orderType: GetMarketsRegionIdOrdersOrderTypeEnum
 }
 
 export async function getJitaSellPrice(typeId: number) {
-    const jitaStation = HUB_STATION_IDS.get("Jita");
-    const sellPrice = (await getOrders(GetMarketsRegionIdOrdersOrderTypeEnum.Sell, typeId, THE_FORGE_REGION_ID))
-        .filter(e => e.locationId == jitaStation)
+    const jitaHub = HUB_STATIONS.get("Jita");
+    if (jitaHub == undefined) {
+        return undefined;
+    }
+    const sellPrice = (await getOrders(GetMarketsRegionIdOrdersOrderTypeEnum.Sell, typeId, jitaHub.regionId))
+        .filter(e => e.locationId == jitaHub?.stationId)
         .map(e => e.price)
         .sort((a, b) => a - b)
         .shift();
