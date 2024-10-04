035c110701494d461d5274efcfc2190593d22dda
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-04 13:03:15 +0900

    feat(ui): Custom error page

---
+++ ui/error.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const error = useError();
+</script>
+
+<template>
+	<div class="flex flex-col h-screen justify-between">
+		<div class="container mb-auto px-4">
+			<h1>{{ error.statusCode }}</h1>
+			<ul>
+				<li v-if="error.url">URL: {{ error.url }}</li>
+				<li v-if="error.statusCode">Status code: {{ error.statusCode }}</li>
+				<li v-if="error.statusMessage">Status message: {{ error.statusMessage }}</li>
+				<li v-if="error.message">Message: {{ error.message }}</li>
+				<li v-if="error.description">Description: {{ error.description }}</li>
+				<li v-if="error.data">Data: {{ error.data }}</li>
+			</ul>
+			<a href="/">Go home &raquo;</a>
+		</div>
+	</div>
+</template>
