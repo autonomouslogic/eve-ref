bd5d00917db79da89490a1d3e270e2aa570e948e
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-04 13:56:29 +0900

    Refactored out market price in a separate component

--- ui/components/cards/market/MarketRow.vue
+++ ui/components/cards/market/MarketRow.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
-import {GetMarketsRegionIdOrdersOrderTypeEnum,} from "~/esi-openapi";
-import UnitValue from "~/components/dogma/UnitValue.vue";
-import {getOrders, type HubStation} from "~/lib/marketUtils";
-import {MONEY} from "~/lib/unitConstants";
+import {type HubStation} from "~/lib/marketUtils";
+import MarketPrice from "~/components/helpers/MarketPrice.vue";
 
 const props = defineProps<{
 	typeId: number,
 	hubStation: HubStation
 }>();
-
-const	sellOrders = await getOrders(GetMarketsRegionIdOrdersOrderTypeEnum.Sell, props.typeId, props.hubStation.regionId);
-const	buyOrders = await getOrders(GetMarketsRegionIdOrdersOrderTypeEnum.Buy, props.typeId, props.hubStation.regionId);
-
-const sellPrice = !sellOrders ? undefined : sellOrders.filter(e => e.locationId == props.hubStation.stationId)
-	.map(e => e.price)
-	.sort((a, b) => a - b)
-	[0];
-const buyPrice = !buyOrders ? undefined : buyOrders.filter(e => e.locationId == props.hubStation.stationId)
-	.map(e => e.price)
-	.sort((a, b) => -(a - b))
-	[0];
 
 </script>
 
@@ -27,12 +13,10 @@ const buyPrice = !buyOrders ? undefined : buyOrders.filter(e => e.locationId == 
 	<tr>
 		<td>{{ hubStation.systemName }}</td>
 		<td class="text-right">
-			<UnitValue v-if="sellPrice" :unit-id="MONEY" :value="sellPrice" />
-			<span v-else>-</span>
+			<MarketPrice :type-id="typeId" order-type="sell" :region-id="hubStation.regionId" :station-id="hubStation.stationId" />
 		</td>
 		<td class="text-right">
-			<UnitValue v-if="buyPrice" :unit-id="MONEY" :value="buyPrice" />
-			<span v-else>-</span>
+			<MarketPrice :type-id="typeId" order-type="buy" :region-id="hubStation.regionId" :station-id="hubStation.stationId" />
 		</td>
 	</tr>
 </template>
