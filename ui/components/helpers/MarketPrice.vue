54f774bdf658edb8720f4dbe1daf5535ce183369
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-04 14:07:33 +0900

    Made sure market price is always client-only

--- ui/components/helpers/MarketPrice.vue
+++ ui/components/helpers/MarketPrice.vue
@@ -3,43 +3,20 @@ import {GetMarketsRegionIdOrdersOrderTypeEnum,} from "~/esi-openapi";
 import UnitValue from "~/components/dogma/UnitValue.vue";
 import {getOrders, HUB_STATIONS} from "~/lib/marketUtils";
 import {MONEY} from "~/lib/unitConstants";
+import MarketCardInner from "~/components/cards/market/MarketCardInner.vue";
+import MarketPriceInner from "~/components/helpers/MarketPriceInner.vue";
 
 interface Props {
 	typeId: number,
-	orderType: "sell" | "buy"
-	regionId: number,
-	stationId: number
+	orderType?: "sell" | "buy"
+	regionId?: number,
+	stationId?: number
 }
-const props = withDefaults(defineProps<Props>(), {
-	orderType: "sell",
-	regionId: HUB_STATIONS.get("Jita")?.regionId,
-	stationId: HUB_STATIONS.get("Jita")?.stationId,
-});
-
-let orderTypeEnum: GetMarketsRegionIdOrdersOrderTypeEnum;
-let comparator: (a: number, b: number) => number;
-if (props.orderType == "sell") {
-	orderTypeEnum = GetMarketsRegionIdOrdersOrderTypeEnum.Sell;
-	comparator = (a: number, b: number) => a - b;
-}
-else if (props.orderType == "buy") {
-	orderTypeEnum = GetMarketsRegionIdOrdersOrderTypeEnum.Buy;
-	comparator = (a: number, b: number) => -(a - b);
-}
-else {
-	throw new Error(`Invalid order type: ${props.orderType}`);
-}
-
-const	allOrders = await getOrders(orderTypeEnum, props.typeId, props.regionId);
-const stationOrders = !allOrders ? undefined : allOrders
-	.filter(e => e.locationId == props.stationId);
-const price = !stationOrders ? undefined : stationOrders
-	.map(e => e.price)
-	.sort(comparator)
-	[0];
+const props = withDefaults(defineProps<Props>(), {});
 </script>
 
 <template>
-	<UnitValue v-if="price" :unit-id="MONEY" :value="price" />
-	<span v-else>-</span>
+	<ClientOnly>
+		<MarketPriceInner :type-id="typeId" :order-type="orderType" :region-id="regionId" :station-id="stationId" />
+	</ClientOnly>
 </template>
