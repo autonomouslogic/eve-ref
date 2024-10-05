f22039605169543a95cd5d78934939c06d6a4806
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-05 12:43:43 +0900

    Sort root categories

--- ui/pages/categories/index.vue
+++ ui/pages/categories/index.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import refdataApi from "~/refdata";
 import CategoryLink from "~/components/helpers/CategoryLink.vue";
+import {tr} from "~/lib/translate";
+const {locale} = useI18n();
 
-const categoryIds: number[] = await refdataApi.getAllCategories();
 useHead({
 	title: "Categories"
 });
+
+const categoryIds: number[] = await refdataApi.getAllCategories();
+const categories = await Promise.all(categoryIds.map(async (categoryId) => await refdataApi.getCategory({categoryId})));
+const sortedCategoryIds = computed(() => categories.sort((a, b) => {
+	const an = tr(a.name, locale.value) || "";
+	const bn = tr(b.name, locale.value) || "";
+	return an.localeCompare(bn);
+})
+	.map((category) => category.categoryId));
+
 </script>
 
 <template>
@@ -14,7 +25,7 @@ useHead({
 		<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			<CategoryLink
 				class="py-2"
-				v-for="categoryId in categoryIds"
+				v-for="categoryId in sortedCategoryIds"
 				:key="categoryId"
 				:categoryId="categoryId">
 			</CategoryLink>
