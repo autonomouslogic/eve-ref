ad9125749393de613f2edf668cc390d459124377
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-05 10:55:30 +0900

    Group pages should use group bundles. Basic sorting on group pages.

--- ui/pages/groups/[groupId].vue
+++ ui/pages/groups/[groupId].vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import refdataApi from "~/refdata";
+import refdataApi, {cacheGroupBundle} from "~/refdata";
 import {getIntRouteParam} from "~/lib/routeUtils";
 import CategoryLink from "~/components/helpers/CategoryLink.vue";
 import {getGroupDogma} from "~/lib/mainDogmaAttributes";
@@ -10,25 +10,35 @@ const {locale} = useI18n();
 
 const groupId = getIntRouteParam(route, "groupId");
 
+await cacheGroupBundle(groupId);
 const group = await refdataApi.getGroup({groupId});
 useHead({
 	title: tr(group.name, locale.value)
 });
-const typeIds: number[] = group.typeIds?.filter((typeId) => typeId !== undefined) as number[];
 const attributes = await getGroupDogma(groupId);
+
+const typeIds: number[] = group.typeIds?.filter((typeId) => typeId !== undefined) as number[];
+const types = await Promise.all(typeIds.map(async (typeId) => await refdataApi.getType({typeId})));
+const sortedTypeIds = computed(() => types.sort((a, b) => {
+	const an = tr(a.name, locale.value) || "";
+	const bn = tr(b.name, locale.value) || "";
+	return an.localeCompare(bn);
+})
+	.map((type) => type.typeId));
 </script>
 
 <template>
 	<div>
-		<h1 v-if="group.name">{{ tr(group.name, locale) }}p</h1>
+		<h1 v-if="group.name">{{ tr(group.name, locale) }}</h1>
 		<div class="mb-3">
 			Category: <CategoryLink :category-id="group.categoryId" />
 		</div>
 		<CompareTable
-			:type-ids="typeIds"
+			:type-ids="sortedTypeIds"
 			:dogma-attribute-names="attributes || []"
 			direction="vertical"
 			:compact-attribute-names="true"
-			:show-meta-group="true"/>
+			:show-meta-group="true"
+			:load-bundles="false" />
 	</div>
 </template>
