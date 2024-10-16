eaa8756a6621872d461035f908ddbc1ed9c1bd7d
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-16 11:09:28 +0900

    feat(ui): Added volume to planetary schematics

--- ui/components/cards/PlanetaryInteractionCard.vue
+++ ui/components/cards/PlanetaryInteractionCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {type DogmaAttribute, type InventoryType, type Schematic} from "~/refdata-openapi";
+import {type BlueprintMaterial, type DogmaAttribute, type InventoryType, type Schematic} from "~/refdata-openapi";
 import CardWrapper from "~/components/cards/CardWrapper.vue";
 import TypeLink from "~/components/helpers/TypeLink.vue";
 import FormattedNumber from "~/components/helpers/FormattedNumber.vue";
@@ -8,6 +8,8 @@ import Duration from "~/components/dogma/units/Duration.vue";
 import AttributeList from "~/components/attr/AttributeList.vue";
 import DogmaListItems from "~/components/cards/DogmaListItems.vue";
 import AttributeListItem from "~/components/attr/AttributeListItem.vue";
+import {CUBIC_METER} from "~/lib/unitConstants";
+import UnitValue from "~/components/dogma/UnitValue.vue";
 
 const props = defineProps<{
 	title: string,
@@ -39,11 +41,50 @@ if (props.inventoryType.installableSchematicIds) {
 	}
 }
 
+const typeVolumes: {[key: string]: number} = {};
+
+async function loadMaterialVolume(material: BlueprintMaterial) {
+	if (material.typeId) {
+		const type = await refdataApi.getType({typeId: material.typeId});
+		if (type.volume) {
+			typeVolumes[material.typeId] = type.volume;
+		}
+	}
+}
+
+for (const schematic of producedBySchematics.values()) {
+	if (schematic.products) {
+		for (let materialId in schematic.products) {
+			const material = schematic.products[materialId];
+			await loadMaterialVolume(material);
+		}
+	}
+	if (schematic.materials) {
+		for (let materialId in schematic.materials) {
+			const material = schematic.materials[materialId];
+			await loadMaterialVolume(material);
+		}
+	}
+}
+
+
+
+// for (let schematic of producedBySchematics) {
+//   Object.entries(schematic.products);
+//   if (schematic.products) {
+//     for (let product of schematic.products) {
+//       if (product.typeId && product.quantity) {
+//         typeVolumes[product.typeId] = product.quantity * typeVolumes[product.typeId];
+//       }
+//     }
+//   }
+// }
+
+
 </script>
 
 <template>
 	<template v-if="dogmaAttributes.length > 0 || producedBySchematics.length > 0 || inventoryType.harvestedByPinTypeIds || usedBySchematics.length > 0 || installableSchematicIds.length > 0 || inventoryType.buildablePinTypeIds">
-
 		<CardWrapper :title="title">
 			<AttributeList :elements="dogmaAttributes.length">
 				<DogmaListItems :inventory-type="inventoryType" :dogma-attributes="dogmaAttributes" />
@@ -70,11 +111,19 @@ if (props.inventoryType.installableSchematicIds) {
 							<tr>
 								<th>Product</th>
 								<th class="text-right">Quantity</th>
+								<th class="text-right">Volume</th>
 							</tr>
 						</thead>
 						<tr v-for="(product, productId) in schematic.products" :key="productId">
 							<td><TypeLink :type-id="product.typeId" /></td>
-							<td class="text-right"><FormattedNumber :number="product.quantity" /></td>
+							<td class="text-right">
+								<FormattedNumber :number="product.quantity" />
+							</td>
+							<td class="text-right">
+								<template v-if="product.typeId && product.quantity && typeVolumes[product.typeId]">
+									<UnitValue :unit-id="CUBIC_METER" :value="product.quantity * typeVolumes[product.typeId]" />
+								</template>
+							</td>
 						</tr>
 					</table>
 
@@ -83,11 +132,21 @@ if (props.inventoryType.installableSchematicIds) {
 							<tr>
 								<th>Material</th>
 								<th class="text-right">Quantity</th>
+								<th class="text-right">Volume</th>
 							</tr>
 						</thead>
 						<tr v-for="(material, materialId) in schematic.materials" :key="materialId">
-							<td><TypeLink :type-id="material.typeId" /></td>
-							<td class="text-right"><FormattedNumber :number="material.quantity" /></td>
+							<td>
+								<TypeLink :type-id="material.typeId" />
+							</td>
+							<td class="text-right">
+								<FormattedNumber :number="material.quantity" />
+							</td>
+							<td class="text-right">
+								<template v-if="material.typeId && material.quantity && typeVolumes[material.typeId]">
+									<UnitValue :unit-id="CUBIC_METER" :value="material.quantity * typeVolumes[material.typeId]" />
+								</template>
+							</td>
 						</tr>
 					</table>
 
@@ -98,7 +157,9 @@ if (props.inventoryType.installableSchematicIds) {
 							</tr>
 						</thead>
 						<tr v-for="pinTypeId in schematic.pinTypeIds" :key="pinTypeId">
-							<td><TypeLink :type-id="pinTypeId" /></td>
+							<td>
+								<TypeLink :type-id="pinTypeId" />
+							</td>
 						</tr>
 					</table>
 
