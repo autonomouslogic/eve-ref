fbd783abbc74545c5f2de9512a540d673c4e2987
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-11-02 09:25:29 +0900

    Building decorator

--- src/main/java/com/autonomouslogic/everef/cli/refdata/post/IndustryModifierSourcesDecorator.java
+++ src/main/java/com/autonomouslogic/everef/cli/refdata/post/IndustryModifierSourcesDecorator.java
@@ -1,15 +1,15 @@
 package com.autonomouslogic.everef.cli.refdata.post;
 
-import com.autonomouslogic.everef.refdata.Blueprint;
-import com.autonomouslogic.everef.refdata.BlueprintMaterial;
-import com.autonomouslogic.everef.refdata.UsedInBlueprint;
+import com.autonomouslogic.everef.refdata.InventoryType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.reactivex.rxjava3.core.Completable;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import javax.inject.Inject;
 import lombok.extern.log4j.Log4j2;
 
@@ -31,42 +31,22 @@ public class IndustryModifierSourcesDecorator extends PostDecorator {
 		return Completable.fromAction(() -> {
 			log.info("Decorating industry modifier sources");
 			types = storeHandler.getRefStore("types");
-			blueprints = storeHandler.getRefStore("blueprints");
-			for (var entry : blueprints.entrySet()) {
-				var blueprintJson = (ObjectNode) entry.getValue();
-				var blueprint = objectMapper.convertValue(blueprintJson, Blueprint.class);
-				handleBlueprint(blueprint);
-			}
+
+			// Build an index of category and group IDs to engineering rig type IDs.
+			var categoryRigs = new HashMap<Long, Set<Long>>();
+			var groupRigs = new HashMap<Long, Set<Long>>();
+			types.forEach((typeId, typeJson) -> {
+				var type = objectMapper.convertValue(typeJson, InventoryType.class);
+				indexType(type.getEngineeringRigCategoryIds(), typeId, categoryRigs);
+				indexType(type.getEngineeringRigGroupIds(), typeId, groupRigs);
+			});
 		});
 	}
 
-	private void handleBlueprint(Blueprint blueprint) {
-		for (var activitiesEntry : blueprint.getActivities().entrySet()) {
-			var activity = activitiesEntry.getKey();
-			var materials = Optional.ofNullable(activitiesEntry.getValue().getMaterials())
-					.map(Map::values)
-					.orElse(List.of());
-			for (var material : materials) {
-				addUsedIn(blueprint, activity, material);
-			}
-		}
-	}
-
-	private void addUsedIn(Blueprint blueprint, String activity, BlueprintMaterial material) {
-		var typeId = material.getTypeId();
-		var typeJson = (ObjectNode) types.get(typeId);
-		if (typeJson == null) {
-			return;
-		}
-		var blueprintTypeId = blueprint.getBlueprintTypeId();
-		var usedInJson = objectMapper.valueToTree(UsedInBlueprint.builder()
-				.materialTypeId(typeId)
-				.quantity(material.getQuantity())
-				.activity(activity)
-				.build());
-		typeJson.withObject("used_in_blueprints")
-				.withObject(Long.toString(blueprintTypeId))
-				.put(activity, usedInJson);
-		types.put(typeId, typeJson);
+	private static void indexType(List<Long> types, long typeId, Map<Long, Set<Long>> index) {
+		Optional.ofNullable(types)
+				.ifPresent(categoryIds -> categoryIds.forEach(categoryId -> {
+					index.computeIfAbsent(categoryId, k -> new HashSet<>()).add(typeId);
+				}));
 	}
 }
