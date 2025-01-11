cabcfd8ee4f7cd4d2a7e48d37ad15ae0048a603d
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-01-11 10:20:06 +0900

    feat: Added key generator script

---
+++ src/main/java/com/autonomouslogic/everef/cli/GenerateKeyPair.java
@@ -0,0 +1,27 @@
+package com.autonomouslogic.everef.cli;
+
+import com.autonomouslogic.everef.crypto.EcKeyGenerator;
+import io.reactivex.rxjava3.core.Completable;
+import javax.inject.Inject;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Fetches donations to EVE Ref from the ESI.
+ */
+@Log4j2
+public class GenerateKeyPair implements Command {
+	@Inject
+	protected EcKeyGenerator keyGenerator;
+
+	@Inject
+	protected GenerateKeyPair() {}
+
+	public Completable run() {
+		return Completable.fromAction(() -> {
+			var privateKey = keyGenerator.generatePrivateKey();
+			var publicKey = keyGenerator.generatePublicKey(privateKey);
+			log.info("Private key: {}", privateKey);
+			log.info("Public key: {}", publicKey);
+		});
+	}
+}
