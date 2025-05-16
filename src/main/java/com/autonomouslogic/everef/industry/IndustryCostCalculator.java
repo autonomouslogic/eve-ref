6a996892219bd3322f53bc32130653eab840aaf9
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-05-16 14:42:30 +0900

    Added specialised skills

--- src/main/java/com/autonomouslogic/everef/industry/IndustryCostCalculator.java
+++ src/main/java/com/autonomouslogic/everef/industry/IndustryCostCalculator.java
@@ -19,6 +19,9 @@ import javax.inject.Inject;
 import lombok.NonNull;
 import lombok.Setter;
 
+import static com.autonomouslogic.everef.industry.SkillIndustryBonuses.GLOBAL_TIME_BONUSES;
+import static com.autonomouslogic.everef.industry.SkillIndustryBonuses.SPECIAL_TIME_BONUSES;
+
 public class IndustryCostCalculator {
 	@Inject
 	protected MarketPriceService marketPriceService;
@@ -98,14 +101,58 @@ public class IndustryCostCalculator {
 		var teMod = 1.0 - industryCostInput.getTe() / 100.0;
 		var runs = industryCostInput.getRuns();
 		var industryMod =
-				1.0 - SkillIndustryBonuses.GLOBAL_TIME_BONUSES.get("Industry") * industryCostInput.getIndustry();
+				1.0 - GLOBAL_TIME_BONUSES.get("Industry") * industryCostInput.getIndustry();
 		var advancedIndustryMod = 1.0
-				- SkillIndustryBonuses.GLOBAL_TIME_BONUSES.get("Advanced Industry")
+				- GLOBAL_TIME_BONUSES.get("Advanced Industry")
 						* industryCostInput.getAdvancedIndustry();
-		return Duration.ofSeconds((long) Math.ceil(runs * baseTime * teMod * industryMod * advancedIndustryMod));
+		var specialSkillMod = manufacturingSpecialisedSkillMod(manufacturing);
+		return Duration.ofSeconds((long) Math.ceil(runs * baseTime * teMod * industryMod * advancedIndustryMod * specialSkillMod));
 	}
 
-	//	private double manufacturingSpecialisedSkillMod(BlueprintActivity manufacturing) {}
+	private double manufacturingSpecialisedSkillMod(BlueprintActivity manufacturing) {
+		return sumSkillMod(
+		specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Advanced Small Ship Construction"), industryCostInput.getAdvancedSmallShipConstruction()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Advanced Industrial Ship Construction"), industryCostInput.getAdvancedIndustrialShipConstruction()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Advanced Medium Ship Construction"), industryCostInput.getAdvancedMediumShipConstruction()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Advanced Large Ship Construction"), industryCostInput.getAdvancedLargeShipConstruction()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("High Energy Physics"), industryCostInput.getHighEnergyPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Plasma Physics"), industryCostInput.getPlasmaPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Nanite Engineering"), industryCostInput.getNaniteEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Hydromagnetic Physics"), industryCostInput.getHydromagneticPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Amarr Starship Engineering"), industryCostInput.getAmarrStarshipEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Minmatar Starship Engineering"), industryCostInput.getMinmatarStarshipEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Graviton Physics"), industryCostInput.getGravitonPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Laser Physics"), industryCostInput.getLaserPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Electromagnetic Physics"), industryCostInput.getElectromagneticPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Rocket Science"), industryCostInput.getRocketScience()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Gallente Starship Engineering"), industryCostInput.getGallenteStarshipEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Nuclear Physics"), industryCostInput.getNuclearPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Mechanical Engineering"), industryCostInput.getMechanicalEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Electronic Engineering"), industryCostInput.getElectronicEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Caldari Starship Engineering"), industryCostInput.getCaldariStarshipEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Quantum Physics"), industryCostInput.getQuantumPhysics()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Molecular Engineering"), industryCostInput.getMolecularEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Triglavian Quantum Engineering"), industryCostInput.getTriglavianQuantumEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Advanced Capital Ship Construction"), industryCostInput.getAdvancedCapitalShipConstruction()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Upwell Starship Engineering"), industryCostInput.getUpwellStarshipEngineering()),
+			specialSkillBonus(manufacturing, SPECIAL_TIME_BONUSES.get("Mutagenic Stabilization"), industryCostInput.getMutagenicStabilization())
+			);
+	}
+
+	private double specialSkillBonus(BlueprintActivity manufacturing, SkillIndustryBonuses.SkillBonus bonus, int level) {
+		if (manufacturing.getRequiredSkills().containsKey(bonus.getSkillId())) {
+			return bonus.getBonus() * level;
+		}
+		return 0.0;
+	}
+
+	private double sumSkillMod(double... bonuses) {
+		var mod = 1.0;
+		for (double bonus : bonuses) {
+			mod *= 1.0 - bonus;
+		}
+		return mod;
+	}
 
 	private BigDecimal manufacturingEiv(BlueprintActivity activityCost) {
 		var eiv = BigDecimal.ZERO;
