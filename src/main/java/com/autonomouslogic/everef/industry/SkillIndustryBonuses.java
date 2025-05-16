ba18aeafdbc24bfa112bde70bedd6532489e2e86
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-05-16 10:45:52 +0900

    Specialised skills

--- src/main/java/com/autonomouslogic/everef/industry/SkillIndustryBonuses.java
+++ src/main/java/com/autonomouslogic/everef/industry/SkillIndustryBonuses.java
@@ -5,74 +5,85 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.Value;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SkillIndustryBonuses {
 	public static final Map<String, Double> GLOBAL_TIME_BONUSES;
+	public static final Map<String, SkillBonus> SPECIAL_TIME_BONUSES;
+
+	@Value
+	public static class SkillBonus {
+		long skillId;
+		double bonus;
+	}
 
 	static {
 		// @todo this can be loaded from Dogma
-		var bonuses = new HashMap<String, Double>();
+		var global = new HashMap<String, Double>();
 		// https://everef.net/types/3380
-		bonuses.put("Industry", 0.04);
+		global.put("Industry", 0.04);
 		// https://everef.net/types/3403
-		bonuses.put("Research", 0.05);
+		global.put("Research", 0.05);
 		// https://everef.net/types/3402
-		bonuses.put("Science", 0.05);
+		global.put("Science", 0.05);
 		// https://everef.net/types/3388
-		bonuses.put("Advanced Industry", 0.03);
+		global.put("Advanced Industry", 0.03);
 		// https://everef.net/types/3409
-		bonuses.put("Metallurgy", 0.05);
+		global.put("Metallurgy", 0.05);
+		GLOBAL_TIME_BONUSES = Collections.unmodifiableMap(global);
+
+		var special = new HashMap<String, SkillBonus>();
 		// https://everef.net/types/3395
-		bonuses.put("Advanced Small Ship Construction", 0.01);
+		special.put("Advanced Small Ship Construction", new SkillBonus(3395, 0.01));
 		// https://everef.net/types/3396
-		bonuses.put("Advanced Industrial Ship Construction", 0.01);
+		special.put("Advanced Industrial Ship Construction", new SkillBonus(3396, 0.01));
 		// https://everef.net/types/3397
-		bonuses.put("Advanced Medium Ship Construction", 0.01);
+		special.put("Advanced Medium Ship Construction", new SkillBonus(3397, 0.01));
 		// https://everef.net/types/3398
-		bonuses.put("Advanced Large Ship Construction", 0.01);
-		// https://everef.net/types/1143
-		bonuses.put("High Energy Physics", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Plasma Physics", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Nanite Engineering", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Hydromagnetic Physics", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Amarr Starship Engineering", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Minmatar Starship Engineering", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Graviton Physics", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Laser Physics", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Electromagnetic Physics", 0.01);
-		// https://everef.net/types/1144
-		bonuses.put("Rocket Science", 0.01);
-		// https://everef.net/types/1145
-		bonuses.put("Gallente Starship Engineering", 0.01);
-		// https://everef.net/types/1145
-		bonuses.put("Nuclear Physics", 0.01);
-		// https://everef.net/types/1145
-		bonuses.put("Mechanical Engineering", 0.01);
-		// https://everef.net/types/1145
-		bonuses.put("Electronic Engineering", 0.01);
-		// https://everef.net/types/1145
-		bonuses.put("Caldari Starship Engineering", 0.01);
-		// https://everef.net/types/1145
-		bonuses.put("Quantum Physics", 0.01);
-		// https://everef.net/types/1152
-		bonuses.put("Molecular Engineering", 0.01);
-		// https://everef.net/types/5230
-		bonuses.put("Triglavian Quantum Engineering", 0.01);
-		// https://everef.net/types/7772
-		bonuses.put("Advanced Capital Ship Construction", 0.01);
-		// https://everef.net/types/8105
-		bonuses.put("Upwell Starship Engineering", 0.01);
-		// https://everef.net/types/8189
-		bonuses.put("Mutagenic Stabilization", 0.0);
-		GLOBAL_TIME_BONUSES = Collections.unmodifiableMap(bonuses);
+		special.put("Advanced Large Ship Construction", new SkillBonus(3398, 0.01));
+		// https://everef.net/types/11433
+		special.put("High Energy Physics", new SkillBonus(11433, 0.01));
+		// https://everef.net/types/11441
+		special.put("Plasma Physics", new SkillBonus(11441, 0.01));
+		// https://everef.net/types/11442
+		special.put("Nanite Engineering", new SkillBonus(11442, 0.01));
+		// https://everef.net/types/11443
+		special.put("Hydromagnetic Physics", new SkillBonus(11443, 0.01));
+		// https://everef.net/types/11444
+		special.put("Amarr Starship Engineering", new SkillBonus(11444, 0.01));
+		// https://everef.net/types/11445
+		special.put("Minmatar Starship Engineering", new SkillBonus(11445, 0.01));
+		// https://everef.net/types/11446
+		special.put("Graviton Physics", new SkillBonus(11446, 0.01));
+		// https://everef.net/types/11447
+		special.put("Laser Physics", new SkillBonus(11447, 0.01));
+		// https://everef.net/types/11448
+		special.put("Electromagnetic Physics", new SkillBonus(11448, 0.01));
+		// https://everef.net/types/11449
+		special.put("Rocket Science", new SkillBonus(11449, 0.01));
+		// https://everef.net/types/11450
+		special.put("Gallente Starship Engineering", new SkillBonus(11450, 0.01));
+		// https://everef.net/types/11451
+		special.put("Nuclear Physics", new SkillBonus(11451, 0.01));
+		// https://everef.net/types/11452
+		special.put("Mechanical Engineering", new SkillBonus(11452, 0.01));
+		// https://everef.net/types/11453
+		special.put("Electronic Engineering", new SkillBonus(11453, 0.01));
+		// https://everef.net/types/11454
+		special.put("Caldari Starship Engineering", new SkillBonus(11454, 0.01));
+		// https://everef.net/types/11455
+		special.put("Quantum Physics", new SkillBonus(11455, 0.01));
+		// https://everef.net/types/11529
+		special.put("Molecular Engineering", new SkillBonus(11529, 0.01));
+		// https://everef.net/types/52307
+		special.put("Triglavian Quantum Engineering", new SkillBonus(52307, 0.01));
+		// https://everef.net/types/77725
+		special.put("Advanced Capital Ship Construction", new SkillBonus(77725, 0.01));
+		// https://everef.net/types/81050
+		special.put("Upwell Starship Engineering", new SkillBonus(81050, 0.01));
+		// https://everef.net/types/81896
+		special.put("Mutagenic Stabilization", new SkillBonus(81896, 0.02));
+		SPECIAL_TIME_BONUSES = Collections.unmodifiableMap(special);
 	}
 }
