e71191890226665bdb38d7fc4bd2df96cb9c09bf
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-05-18 00:17:30 +0900

    chore: Script for fetching ESI prices for blueprints (#821)

---
+++ scripts/esi-prices.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+ids=()
+for id in "$@"; do
+  body="$(curl -sS --fail https://ref-data.everef.net/blueprints/$id)"
+  extracted="$(echo "$body" | jq '.activities[] | [ .materials, .products ][] | select(. != null) | .[].type_id')"
+  for i in $extracted; do
+    ids+=($i)
+  done
+done
+
+query=()
+for id in ${ids[*]}; do
+  query+=(".type_id==$id")
+done
+joined=$(printf " or %s" "${query[@]}")
+joined=${joined:4}
+
+curl -sS --fail https://esi.evetech.net/latest/markets/prices/ | jq "[.[] | select($joined)]"
