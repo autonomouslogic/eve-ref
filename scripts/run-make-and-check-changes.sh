716fc8e8efd3e163ca24cef3fbc6e724cbfecef1
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-06-03 00:47:01 +0900

    Env vars

--- scripts/run-make-and-check-changes.sh
+++ scripts/run-make-and-check-changes.sh
@@ -6,16 +6,15 @@
 COMMAND=$1
 URL=$2
 
-
 curl -fsS -m 10 --retry 5 -o /dev/null \
   -X POST -H "Content-Type: application/json" \
   --data "{\"content\": \"${COMMAND} test\"}" \
-  $DEV_NOTICES_DISCORD_WEBHOOK_URL
+  $URL
 
 make $COMMAND
 if [[ `git status --porcelain` ]]; then
   curl -fsS -m 10 --retry 5 -o /dev/null \
     -X POST -H "Content-Type: application/json" \
     --data "{\"content\": \"${COMMAND} need to be updated\"}" \
-    $DEV_NOTICES_DISCORD_WEBHOOK_URL
+    $URL
 fi
