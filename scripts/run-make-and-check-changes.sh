2e71c5aa1725276ad551b40f69e5cf8153d10c7e
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-06-03 00:54:10 +0900

    Removed push

--- scripts/run-make-and-check-changes.sh
+++ scripts/run-make-and-check-changes.sh
@@ -6,11 +6,6 @@
 COMMAND=$1
 URL=$2
 
-curl -fsS -m 10 --retry 5 -o /dev/null \
-  -X POST -H "Content-Type: application/json" \
-  --data "{\"content\": \"${COMMAND} test\"}" \
-  $URL
-
 make $COMMAND
 if [[ `git status --porcelain` ]]; then
   curl -fsS -m 10 --retry 5 -o /dev/null \
