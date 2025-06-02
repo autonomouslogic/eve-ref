83be1d2626ad8940012e64cd75ac55202bb44ba4
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-06-03 00:40:18 +0900

    Moved to external script

---
+++ scripts/run-make-and-check-changes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# Runs a make command and calls a Discord webhook if there are any changes.
+# Used by Github Actions.
+
+COMMAND=$1
+URL=$2
+
+
+curl -fsS -m 10 --retry 5 -o /dev/null \
+  -X POST -H "Content-Type: application/json" \
+  --data "{\"content\": \"${COMMAND} test\"}" \
+  $DEV_NOTICES_DISCORD_WEBHOOK_URL
+
+make $COMMAND
+if [[ `git status --porcelain` ]]; then
+  curl -fsS -m 10 --retry 5 -o /dev/null \
+    -X POST -H "Content-Type: application/json" \
+    --data "{\"content\": \"${COMMAND} need to be updated\"}" \
+    $DEV_NOTICES_DISCORD_WEBHOOK_URL
+fi
