21e0ab52bc24c49ec8c6f08452a0d9487b5b324c
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-06-07 01:17:29 +0900

    Working on which blueprints types are used in

--- ui/components/cards/IndustryCard.vue
+++ ui/components/cards/IndustryCard.vue
@@ -2,6 +2,7 @@
 import {type DogmaAttribute, type InventoryType} from "~/refdata-openapi";
 import CardWrapper from "~/components/cards/CardWrapper.vue";
 import ExternalLink from "~/components/helpers/ExternalLink.vue";
+import InternalLink from "~/components/helpers/InternalLink.vue";
 
 const props = defineProps<{
 	title: string,
@@ -16,8 +17,17 @@ const showCard = computed(() => {
 
 <template>
 	<CardWrapper :title="title" v-if="showCard">
-		<ExternalLink
-			v-if="!inventoryType.isBlueprint"
-			:url="`https://www.adam4eve.eu/material_influence.php?material=${inventoryType.typeId}`">Adam4Eve Material Influence</ExternalLink>
+		<ul>
+			<li>
+				<InternalLink :to="`/types/${inventoryType.typeId}/used-in`">
+					View blueprints
+				</InternalLink>
+			</li>
+			<li>
+				<ExternalLink
+					v-if="!inventoryType.isBlueprint"
+					:url="`https://www.adam4eve.eu/material_influence.php?material=${inventoryType.typeId}`">Adam4Eve Material Influence</ExternalLink>
+			</li>
+		</ul>
 	</CardWrapper>
 </template>
---
+++ ui/pages/types/[typeId]/used-in.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import refdataApi, {cacheTypeBundle} from "~/refdata";
+import {type InventoryGroup} from "~/refdata-openapi";
+import CategoryLink from "~/components/helpers/CategoryLink.vue";
+import GroupLink from "~/components/helpers/GroupLink.vue";
+import MarketGroupBreadcrumbs from "~/components/helpers/MarketGroupBreadcrumbs.vue";
+import TypeCards from "~/components/types/TypeCards.vue";
+import LinkParser from "~/components/helpers/LinkParser.vue";
+import {getIntRouteParam} from "~/lib/routeUtils";
+import {tr} from "~/lib/translate";
+import EveImage from "~/components/icons/EveImage.vue";
+import {getTypeIconUrl} from "~/lib/urls";
+import {SHIP} from "~/lib/categoryConstants";
+import ExternalLink from "~/components/helpers/ExternalLink.vue";
+import TypeLink from "~/components/helpers/TypeLink.vue";
+
+const {locale} = useI18n();
+const route = useRoute();
+const typeId: number = getIntRouteParam(route, "typeId");
+if (!typeId) {
+	console.error("typeId is null");
+}
+
+await cacheTypeBundle(typeId);
+
+const inventoryType = await refdataApi.getType({typeId});
+if (inventoryType == undefined) {
+	throw new Error(`Inventory type ${typeId} not found`);
+}
+
+const pageTitle = tr(inventoryType.name, locale.value);
+
+useHead({
+	title: `Blueprints using ${pageTitle}`
+});
+</script>
+
+<template>
+	<h1 v-if="inventoryType.name" class="mb-3">
+		Blueprints using {{ tr(inventoryType.name, locale) }}
+	</h1>
+
+	<ClientOnly>
+		<div v-if="!inventoryType.usedInBlueprints">
+			{{ tr(inventoryType.name, locale) }} isn't used in any blueprints.
+		</div>
+		<table v-else class="standard-table">
+			<thead>
+				<tr>
+					<th>Blueprint</th>
+					<th>Activity</th>
+					<th class="text-right">Quantity</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(activity, blueprintId) in inventoryType.usedInBlueprints" :key="blueprintId">
+					<td>
+						<TypeLink :type-id="blueprintId" />
+					</td>
+					<td>{{ activity }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</ClientOnly>
+</template>
