77090c664fdd5a89370abd226e94573a33d1d0a2
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-06-07 09:59:47 +0900

    feat(ui): Pages showing which blueprints a given type is used in (#908)

--- ui/components/cards/IndustryCard.vue
+++ ui/components/cards/IndustryCard.vue
@@ -2,6 +2,7 @@
 import {type DogmaAttribute, type InventoryType} from "~/refdata-openapi";
 import CardWrapper from "~/components/cards/CardWrapper.vue";
 import ExternalLink from "~/components/helpers/ExternalLink.vue";
+import InternalLink from "~/components/helpers/InternalLink.vue";
 
 const props = defineProps<{
 	title: string,
@@ -16,8 +17,23 @@ const showCard = computed(() => {
 
 <template>
 	<CardWrapper :title="title" v-if="showCard">
-		<ExternalLink
-			v-if="!inventoryType.isBlueprint"
-			:url="`https://www.adam4eve.eu/material_influence.php?material=${inventoryType.typeId}`">Adam4Eve Material Influence</ExternalLink>
+		<ul class="list-disc list-inside">
+			<li>
+				<template v-if="inventoryType.usedInBlueprints && Object.keys(inventoryType.usedInBlueprints).length > 0">
+					<InternalLink
+						:to="`/types/${inventoryType.typeId}/used-in`">
+						View blueprints
+					</InternalLink> ({{ Object.keys(inventoryType.usedInBlueprints).length }})
+				</template>
+				<template v-else>
+					Not used in any blueprints
+				</template>
+			</li>
+			<li>
+				<ExternalLink
+					v-if="!inventoryType.isBlueprint"
+					:url="`https://www.adam4eve.eu/material_influence.php?material=${inventoryType.typeId}`">Adam4Eve Material Influence</ExternalLink>
+			</li>
+		</ul>
 	</CardWrapper>
 </template>
