c89a5912a230126e6ef6e2bf2635a532c642d7c0
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-06-24 22:07:37 +0900

    Implemented custom toUrlQueryString and map handling that doesn't mess with the OpenAPI spec

--- src/main/java/com/autonomouslogic/everef/model/api/IndustryPrices.java
+++ src/main/java/com/autonomouslogic/everef/model/api/IndustryPrices.java
@@ -5,15 +5,16 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.SchemaProperties;
 import lombok.experimental.Delegate;
 import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringJoiner;
 
 @Schema
 public class IndustryPrices {
-	@Delegate
 	private final Map<String, BigDecimal> prices = new HashMap<>();
 
 	public String toUrlQueryString(@NonNull String prefix) {
@@ -22,27 +23,34 @@ public class IndustryPrices {
 		// https://github.com/OpenAPITools/openapi-generator/issues/16183
 		// https://swagger.io/docs/specification/v3_0/data-models/dictionaries/
 
-		String suffix = "";
-		String containerSuffix = "";
-		String containerPrefix = "";
-		if (prefix == null) {
-			// style=form, explode=true, e.g. /pet?name=cat&type=manx
-			prefix = "";
-		} else {
-			// deepObject style e.g. /pet?id[name]=cat&id[type]=manx
-			prefix = prefix + "[";
-			suffix = "]";
-			containerSuffix = "]";
-			containerPrefix = "[";
+		if (StringUtils.isEmpty(prefix)) {
+			throw new IllegalArgumentException("prefix must not be null or empty");
 		}
 
 		var joiner = new StringJoiner("&");
+		for (var entry : entrySet()) {
+			var key = entry.getKey();
+			var value = entry.getValue().toString();
+			joiner.add(String.format("%s[%s]=%s", prefix,
 
-		// add `alpha_clone_tax` to the URL query string
-//		if (getAlphaCloneTax() != null) {
-//			joiner.add(String.format("%salpha_clone_tax%s=%s", prefix, suffix, ApiClient.urlEncode(ApiClient.valueToString(getAlphaCloneTax()))));
-//		}
+				ApiClient.urlEncode(ApiClient.valueToString(key)),
+				ApiClient.urlEncode(ApiClient.valueToString(value))
+
+			));
+		}
 
 		return joiner.toString();
+	}
+
+	public BigDecimal get(Object key) {
+		return prices.get(key);
+	}
+
+	public BigDecimal put(String key, BigDecimal value) {
+		return prices.put(key, value);
+	}
+
+	public Set<Map.Entry<String, BigDecimal>> entrySet() {
+		return prices.entrySet();
 	}
 }
