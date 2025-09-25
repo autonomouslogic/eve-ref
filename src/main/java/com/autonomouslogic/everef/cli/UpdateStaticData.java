fd4d2ff4f35ace37352d23be73b8490667110ff9
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2025-09-26 01:15:40 +0900

    Sync changelog too

--- src/main/java/com/autonomouslogic/everef/cli/UpdateStaticData.java
+++ src/main/java/com/autonomouslogic/everef/cli/UpdateStaticData.java
@@ -12,14 +12,17 @@ import com.autonomouslogic.everef.util.DataIndexHelper;
 import com.autonomouslogic.everef.util.TempFiles;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.attribute.FileTime;
 import java.time.Duration;
+import java.time.Instant;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.io.FilenameUtils;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 /**
@@ -56,9 +59,10 @@ public class UpdateStaticData implements Command {
 	private final Duration archiveCacheTime = Configs.DATA_ARCHIVE_CACHE_CONTROL_MAX_AGE.getRequired();
 
 	private S3Url dataPath;
-	//	private HttpUrl dataUrl;
 	private final URI staticDataUrl =
 			URI.create("https://developers.eveonline.com/static-data/tranquility/latest.jsonl");
+	private final URI schemaUrl =
+			URI.create("https://developers.eveonline.com/static-data/tranquility/schema-changelog.yaml");
 
 	@Inject
 	protected UpdateStaticData() {}
@@ -74,6 +78,7 @@ public class UpdateStaticData implements Command {
 		var latest = loadLatestFiles();
 		syncFile(latest, ArchivePathFactory.STATIC_DATA_JSONL, "jsonl");
 		syncFile(latest, ArchivePathFactory.STATIC_DATA_YAML, "yaml");
+		syncSchema();
 	}
 
 	@SneakyThrows
@@ -93,6 +98,15 @@ public class UpdateStaticData implements Command {
 	@SneakyThrows
 	private void syncFile(StaticDataMeta latest, ArchivePathFactory type, String variant) {
 		var latestUri = dataUrl(latest.getBuildNumber(), variant);
+
+		var latestPath = getArchivePath(latest.getReleaseDate(), type, latest.getBuildNumber());
+		var existing =
+				s3Adapter.listObjects(latestPath, false, s3Client).toList().blockingGet();
+		if (!existing.isEmpty()) {
+			log.info(String.format("Latest file already exists at %s", latestPath));
+			return;
+		}
+
 		var file = tempFiles.tempFile("sde-" + variant, ".zip").toFile();
 		var response = okHttpWrapper.download(latestUri.toString(), file);
 		if (response.code() != 200) {
@@ -109,11 +123,8 @@ public class UpdateStaticData implements Command {
 	@SneakyThrows
 	private void uploadFile(File outputFile, ArchivePathFactory type, int buildNumber) {
 		var latestPath = dataPath.resolve(type.createLatestPath());
-		var archivePathBase = type.createArchivePath(
-				Files.getLastModifiedTime(outputFile.toPath()).toInstant());
-		archivePathBase =
-				archivePathBase.replace("eve-online-static-data-", "eve-online-static-data-" + buildNumber + "-");
-		var archivePath = dataPath.resolve(archivePathBase);
+		var filetime = Files.getLastModifiedTime(outputFile.toPath()).toInstant();
+		var archivePath = getArchivePath(filetime, type, buildNumber);
 		var latestPut = s3Util.putPublicObjectRequest(outputFile.length(), latestPath, latestCacheTime);
 		var archivePut = s3Util.putPublicObjectRequest(outputFile.length(), archivePath, archiveCacheTime);
 		log.info(String.format("Uploading latest file to %s", latestPath));
@@ -123,9 +134,31 @@ public class UpdateStaticData implements Command {
 		dataIndexHelper.updateIndex(latestPath, archivePath).blockingAwait();
 	}
 
+	private S3Url getArchivePath(Instant filetime, ArchivePathFactory type, int buildNumber) throws IOException {
+		var archivePathBase = type.createArchivePath(filetime);
+		archivePathBase =
+				archivePathBase.replace("eve-online-static-data-", "eve-online-static-data-" + buildNumber + "-");
+		var archivePath = dataPath.resolve(archivePathBase);
+		return archivePath;
+	}
+
 	private URI dataUrl(int buildNumber, String variant) {
 		return URI.create(String.format(
 				"https://developers.eveonline.com/static-data/tranquility/eve-online-static-data-%s-%s.zip",
 				buildNumber, variant));
+	}
+
+	private void syncSchema() {
+		var file = tempFiles.tempFile("schema", ".yaml").toFile();
+		var response = okHttpWrapper.download(schemaUrl.toString(), file);
+		if (response.code() != 200) {
+			throw new RuntimeException("Failed to download " + schemaUrl + ": " + response.code());
+		}
+		var name = FilenameUtils.getName(schemaUrl.getPath());
+		var folder = ArchivePathFactory.STATIC_DATA_JSONL.getFolder();
+		var s3Url = dataPath.resolve(folder + "/" + name);
+		var latestPut = s3Util.putPublicObjectRequest(file.length(), s3Url, latestCacheTime);
+		s3Adapter.putObject(latestPut, file, s3Client).blockingGet();
+		dataIndexHelper.updateIndex(s3Url, s3Url).blockingAwait();
 	}
 }
