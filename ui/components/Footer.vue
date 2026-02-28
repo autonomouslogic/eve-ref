19b26432d4bc51e585d4a2bd22cf568e55bb643d
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2026-02-28 13:25:18 +0900

    fix(ui): Added privacy settings link for Usercentric

--- ui/components/Footer.vue
+++ ui/components/Footer.vue
@@ -28,6 +28,7 @@ import InternalLink from "~/components/helpers/InternalLink.vue";
 					<li><ExternalLink url="https://docs.everef.net/">Docs</ExternalLink></li>
 					<li><ExternalLink url="https://docs.everef.net/datasets/">Data</ExternalLink></li>
 					<li><InternalLink to="/privacy">Privacy</InternalLink></li>
+					<li><a href="#" onClick="__ucCmp.showSecondLayer();">Privacy Settings</a></li>
 					<li>
 						<LocaleSwitcher/>
 					</li>
