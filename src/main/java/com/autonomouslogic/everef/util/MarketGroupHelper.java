ffcda63c973de194afcf3cc16106e9f1e5f1a333
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2026-04-06 10:29:30 +0900

    fix: Nullpointer on BuildSearch (#1031)

--- src/main/java/com/autonomouslogic/everef/util/MarketGroupHelper.java
+++ src/main/java/com/autonomouslogic/everef/util/MarketGroupHelper.java
@@ -1,5 +1,6 @@
 package com.autonomouslogic.everef.util;
 
+import com.autonomouslogic.everef.data.LoadedRefData;
 import com.autonomouslogic.everef.refdata.InventoryType;
 import com.autonomouslogic.everef.refdata.MarketGroup;
 import com.autonomouslogic.everef.service.RefDataService;
@@ -15,26 +16,34 @@ public class MarketGroupHelper {
 	public MarketGroupHelper() {}
 
 	public MarketGroup getRootMarketGroup(InventoryType type) {
+		var loadedRefData = refDataService.getLoadedRefData();
+		return getRootMarketGroup(type, loadedRefData);
+	}
+
+	public MarketGroup getRootMarketGroup(InventoryType type, LoadedRefData loadedRefData) {
 		if (type.getMarketGroupId() == null) {
 			return null;
 		}
-		var loadedRefData = refDataService.getLoadedRefData();
 		var marketGroup = loadedRefData.getMarketGroup(type.getMarketGroupId());
 		if (marketGroup == null) {
 			return null;
 		}
-		return getRootMarketGroup(marketGroup);
+		return getRootMarketGroup(marketGroup, loadedRefData);
 	}
 
 	public MarketGroup getRootMarketGroup(MarketGroup marketGroup) {
+		var loadedRefData = refDataService.getLoadedRefData();
+		return getRootMarketGroup(marketGroup, loadedRefData);
+	}
+
+	public MarketGroup getRootMarketGroup(MarketGroup marketGroup, LoadedRefData loadedRefData) {
 		if (marketGroup.getParentGroupId() == null) {
 			return marketGroup;
 		}
-		var loadedRefData = refDataService.getLoadedRefData();
 		var parentGroup = loadedRefData.getMarketGroup(marketGroup.getParentGroupId());
 		if (parentGroup == null) {
 			return marketGroup;
 		}
-		return getRootMarketGroup(parentGroup);
+		return getRootMarketGroup(parentGroup, loadedRefData);
 	}
 }
