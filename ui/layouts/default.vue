3a8f517d07716fbfdf667c496f4e33fdbb5b33a3
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2026-04-22 13:46:25 +0900

    fix(ui): Centered ads

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -4,21 +4,24 @@
 		<header>
 			<navbar />
 		</header>
-		<Adsbygoogle
-			ad-slot="2103102198"
-			ad-format="auto"
-			:hide-unfilled="true"
-		/>
-		<main class="container mb-auto px-4"><slot/></main>
+		<main class="container mb-auto px-4">
+			<div class="flex w-full justify-center m-0 p-0">
+				<Adsbygoogle
+					ad-slot="2103102198"
+					ad-format="auto"
+					:hide-unfilled="true"
+				/>
+			</div>
+			<slot/>
+		</main>
 		<footer class="mt-4">
 			<Footer/>
 		</footer>
 	</div>
 </template>
 
-
-<style>
-
+<style scoped>
 </style>
+
 <script setup lang="ts">
 </script>
