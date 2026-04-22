29b7a1ed92cd5b3852b37e79ba282bd4ef96e6d6
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2026-04-22 14:05:36 +0900

    fix(ui): Removed fixed ad slot

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -5,13 +5,6 @@
 			<navbar />
 		</header>
 		<main class="container mb-auto px-4">
-			<div class="flex w-full justify-center m-0 p-0">
-				<Adsbygoogle
-					ad-slot="2103102198"
-					ad-format="auto"
-					:hide-unfilled="true"
-				/>
-			</div>
 			<slot/>
 		</main>
 		<footer class="mt-4">
