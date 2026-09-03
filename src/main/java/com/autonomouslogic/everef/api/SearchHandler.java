d130d826395cd9315d6a3f77b764f220f05382af
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2026-09-04 08:46:00 +0900

    fix(api): fixed handling of null search queries (#1099)

--- src/main/java/com/autonomouslogic/everef/api/SearchHandler.java
+++ src/main/java/com/autonomouslogic/everef/api/SearchHandler.java
@@ -30,6 +30,8 @@ import tools.jackson.databind.json.JsonMapper;
 @Path("/v1/search")
 @Log4j2
 public class SearchHandler implements HttpService, Handler {
+	private static final String Q = "q";
+
 	@Inject
 	protected JsonMapper jsonMapper;
 
@@ -59,7 +61,7 @@ public class SearchHandler implements HttpService, Handler {
 	public SearchResult search(
 			@Parameter(
 							in = ParameterIn.QUERY,
-							name = "q",
+							name = Q,
 							description = "Search query (minimum 3 characters)",
 							required = true,
 							schema = @Schema(type = "string"))
@@ -78,7 +80,7 @@ public class SearchHandler implements HttpService, Handler {
 		log.info(
 				"Received request: {}",
 				URLEncoder.encode(req.requestedUri().toUri().toString(), StandardCharsets.UTF_8));
-		var q = req.query().first("q").orElse(null);
+		var q = extractQ(req);
 		SearchResult result;
 		try {
 			result = search(q);
@@ -89,5 +91,21 @@ public class SearchHandler implements HttpService, Handler {
 		res.status(Status.OK_200);
 		apiUtil.setStandardHeaders(res, Duration.ofMinutes(10));
 		res.send(json);
+	}
+
+	private String extractQ(ServerRequest req) {
+		var uriQuery = req.query();
+		if (uriQuery == null || uriQuery.isEmpty()) {
+			throwExactlyOneQ();
+		}
+		var rawValues = uriQuery.getAllRaw(Q);
+		if (rawValues.isEmpty() || rawValues.size() != 1) {
+			throwExactlyOneQ();
+		}
+		return uriQuery.first(Q).get();
+	}
+
+	private static void throwExactlyOneQ() {
+		throw new ClientException("Exactly one " + Q + " parameter expected");
 	}
 }
