573e9e9103433d6cf52b4f28a88b528b90569951
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2026-09-08 01:14:57 +0900

    refactor: Removed RxJava from FlywayMigrate (#1107)

--- src/main/java/com/autonomouslogic/everef/cli/flyway/FlywayMigrate.java
+++ src/main/java/com/autonomouslogic/everef/cli/flyway/FlywayMigrate.java
@@ -4,7 +4,6 @@ import com.autonomouslogic.commons.concurrent.VirtualThreads;
 import com.autonomouslogic.everef.cli.Command;
 import com.autonomouslogic.everef.config.Configs;
 import com.autonomouslogic.everef.db.DbAccess;
-import io.reactivex.rxjava3.core.Completable;
 import javax.inject.Inject;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
@@ -28,12 +27,9 @@ public class FlywayMigrate implements Command {
 		VirtualThreads.onVirtualThread(() -> dbAccess.flyway().migrate());
 	}
 
-	public Completable autoRun() {
-		return Completable.defer(() -> {
-			if (Configs.FLYWAY_AUTO_MIGRATE.getRequired()) {
-				return Completable.fromAction(this::run);
-			}
-			return Completable.complete();
-		});
+	public void autoRun() {
+		if (Configs.FLYWAY_AUTO_MIGRATE.getRequired()) {
+			run();
+		}
 	}
 }
